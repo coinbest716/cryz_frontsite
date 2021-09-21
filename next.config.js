7c7177322fc167a00207671080f1e699669283fe
Nicholas Klave <nicholas.klave716@gmail.com>
2021-09-21 20:13:08 +0200

    Add baseurl feature.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+const webpack = require('webpack')
+const path = require('path')
+
 module.exports = {
   reactStrictMode: true,
+  webpack(config, options) {
+    config.resolve.modules.push(path.resolve('./'))
+    return config
+  },
 }
