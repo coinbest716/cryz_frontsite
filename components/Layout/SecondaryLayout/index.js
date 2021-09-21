95d8423ea155934fbbcab0164397d62d623b6c03
Nicholas Klave <nicholas.klave716@gmail.com>
2021-09-21 20:32:58 +0200

    Add some layout files.

---
+++ components/Layout/SecondaryLayout/index.js
@@ -0,0 +1,14 @@
+import Header from 'components/Header'
+import Navbar from 'components/Navbar'
+import Footer from 'components/Footer'
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
