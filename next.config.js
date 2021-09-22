c9f607a57de2d52250fa8b99cfddd6c6fbc99c37
Nicholas Klave <nicholas.klave716@gmail.com>
2021-09-22 11:00:45 +0200

    Add tailwindCss and sass into project.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
-const webpack = require('webpack')
 const path = require('path')
 
 module.exports = {
+  trailingSlash: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   reactStrictMode: true,
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
   webpack(config, options) {
     config.resolve.modules.push(path.resolve('./'))
     return config
---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Fragment } from 'react'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <Fragment>
+            <script type="text/javascript" src="https://cdn.iubenda.com/cons/iubenda_cons.js" async />
+          </Fragment>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Zilla+Slab:ital@1&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
