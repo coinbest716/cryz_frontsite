ece8d9840a9cdc9a1a094465ad43769341967139
Nicholas Klave <nicholas.klave716@gmail.com>
2021-09-23 06:38:37 +0200

    Update _app.js file with layout.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import 'styles/style.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout || (page => page)
+
+  return getLayout(<Component {...pageProps} />)
 }
 
 export default MyApp
