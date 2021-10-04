bb92e422980ccf072d1c57808482fa500192ccf2
Nicholas Klave <nicholas.klave716@gmail.com>
2021-10-04 19:02:13 +0200

    Update Menu component.

--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -9,15 +9,11 @@ import globalStyles from 'styles/GlobalStyle.module.scss'
 import styles from 'components/Menu/Menu.module.scss'
 
 // images
+import AcademyImage from 'assets/images/team-member-01.png'
 import PinterestIcon from 'assets/images/pinterest.svg'
 import FacebookIcon from 'assets/images/facebook.svg'
 import TwitterIcon from 'assets/images/twitter.svg'
 import LinkedinIcon from 'assets/images/linkedin.svg'
-
-/**
- * rightBar: desktop
- * ribbon: mobile
- */
 
 const BurgerIcon = ({ visibilty, setVisibility, router }) => {
   return (
@@ -29,10 +25,8 @@ const BurgerIcon = ({ visibilty, setVisibility, router }) => {
   )
 }
 
-export default function Menu({ mobileStyle, rightBar = 'green', ribbon = 'green' }) {
+const Menu = () => {
   const [visibility, setVisibility] = useState()
-  const [hoverExpertise, setHoverExpertise] = useState(0)
-  const [hoverSub, setHoverSub] = useState(0)
   const router = useRouter()
 
   return (
@@ -40,23 +34,39 @@ export default function Menu({ mobileStyle, rightBar = 'green', ribbon = 'green'
       <BurgerIcon visibilty={visibility} setVisibility={setVisibility} router={router} />
 
       <div className={visibility === true ? styles.menuOpen : visibility === false ? styles.menuClose : styles.menu}>
-        <div className={globalStyles.container + ' mx-auto'}>
-          <div className={`${styles.menuItems} mt-32 sm:mt-64 sm:mb-10`}>
-            <Link href="/academy">
-              <a id="academy">
-                <div className={`${styles.number} invisible sm:visible mx-1`}>01</div>
-                Academy
-              </a>
-            </Link>
-            <Link href="/news">
-              <a id="news">
-                News
-                <div className={`${styles.number} invisible sm:visible mx-1`}>02</div>
-              </a>
-            </Link>
+        <div className={globalStyles.container + ' mx-auto h-screen flex flex-wrap content-between'}>
+          <div className={styles.menuItems + ' w-full mt-32 sm:mt-48'}>
+            <div className="grid grid-cols-2 cursor-pointer flex items-center h-full">
+              <div className="relative">
+                <div className="opacity-75 -z-10">
+                  <Image src={AcademyImage} alt="" width={482} height={482} />
+                </div>
+                <div className="absolute inset-y-1/4 w-full flex justify-center">
+                  <Link href="/academy">
+                    <a id="academy">
+                      <div className={`${styles.number} invisible sm:visible mx-1`}>01</div>
+                      Academy
+                    </a>
+                  </Link>
+                </div>
+              </div>
+              <div className="relative">
+                <div className="opacity-75 -z-10">
+                  <Image src={AcademyImage} alt="" width={482} height={482} />
+                </div>
+                <div className="absolute inset-y-1/4 w-full flex justify-center">
+                  <Link href="/news">
+                    <a id="news">
+                      News
+                      <div className={`${styles.number} invisible sm:visible mx-1`}>02</div>
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
 
-          <div className={`${styles.socials} flex flex-wrap mr-4`}>
+          <div className={`${styles.socials} w-full flex flex-wrap mr-4`}>
             <div className="w-full flex justify-end">
               <div className="mx-1">
                 <Link href={'https://www.pinterest.com/'}>
@@ -88,7 +98,7 @@ export default function Menu({ mobileStyle, rightBar = 'green', ribbon = 'green'
               </div>
             </div>
             <div className={styles.whiteLine}></div>
-            <div className={`w-full flex justify-end ${styles.copyright}`}>
+            <div className={`w-full flex justify-end mb-10 ${styles.copyright}`}>
               CrysDyaz&Co © Todos los derechos reservados
             </div>
           </div>
@@ -97,3 +107,5 @@ export default function Menu({ mobileStyle, rightBar = 'green', ribbon = 'green'
     </>
   )
 }
+
+export default Menu
