78575402391dc7730bbddb417ec94e299af5f1f7
luming63 <laomingri@gmail.com>
2021-10-06 17:13:02 +0200

    fixed: title font size

--- pages/services/training.js
+++ pages/services/training.js
@@ -1,8 +1,32 @@
 import PrimaryLayout from 'components/Layout/PrimaryLayout'
-import styles from 'styles/Common.module.scss'
+import globlaStyle from 'styles/GlobalStyle.module.scss'
+import styles from './training.module.scss'
+import backGrayIcon from 'assets/images/arrow-left-gray.svg'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 const Training = () => {
-  return <div className={styles.container}>Training page content</div>
+  const router = useRouter()
+
+  const handleClickBack = () => {
+    router.push('/services')
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className="flex flex-wrap justify-center">
+        <div className={globlaStyle.container}>
+          <div className="mt-9">
+            <button className="flex justify-between items-center" onClick={handleClickBack}>
+              <Image src={backGrayIcon} alt="" width={20} height={15} />
+              <p className={styles.back}>&nbsp;&nbsp;Volver</p>
+            </button>
+          </div>
+          <div className={styles.topTitle}>Entrenamiento</div>
+        </div>
+      </div>
+    </div>
+  )
 }
 export default Training
 
