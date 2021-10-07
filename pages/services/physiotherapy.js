1378298cf3b9efd3c087f38ca38a4f9f9cc858dc
luming63 <laomingri@gmail.com>
2021-10-07 10:39:40 +0200

    fixed: physiotherapy text field, background

--- pages/services/physiotherapy.js
+++ pages/services/physiotherapy.js
@@ -1,8 +1,54 @@
 import PrimaryLayout from 'components/Layout/PrimaryLayout'
-import styles from 'styles/Common.module.scss'
+import globlaStyle from 'styles/GlobalStyle.module.scss'
+import styles from './physiotherapy.module.scss'
+import backGrayIcon from 'public/images/arrow-left-gray.svg'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 const Physiotherapy = () => {
-  return <div className={styles.container}>Physiotherapy page content</div>
+  const router = useRouter()
+
+  const handleClickBack = () => {
+    router.push('/services')
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className="flex flex-wrap justify-center pb-20">
+        <div className={globlaStyle.container}>
+          <div className="mt-9">
+            <button className="flex justify-between items-center" onClick={handleClickBack}>
+              <Image src={backGrayIcon} alt="" width={20} height={15} />
+              <p className={styles.back}>&nbsp;&nbsp;Volver</p>
+            </button>
+          </div>
+          <div className="grid grid-cols-12 gap-4">
+            <div className="col-span-12 md:col-span-5 sm:col-span-12 ">
+              <div className={'pt-10 ' + styles.topTitle}>Fisioterapia</div>
+              <div className={styles.topDash} />
+              <div className={styles.topDescription + ' mt-10 pb-20'}>
+                Nuestro equipo de fisioterapia y osteopatía integra el tratamiento de diferentes patologías, así como,
+                dolores y molestias de nuestro día a día para distintos tipos de perfiles: embarazadas, ancianos, bebés,
+                deportistas… En constante formación de las últimas tendencias y metodologías, realizamos una valoración
+                previa del paciente y utilizamos técnicas manuales, además de aparatología como INDIBA, ya sea lesiones
+                como postparto, kinesiotaping, punción seca...
+                <br />
+                Algunos de nuestros tratamientos son la terapia manual, terapia activa, osteopatía, INDIBA, crioterapia,
+                termoterapia, electroterapia (TENS)... <br />
+                Todo ello para mejorar tu bienestar y llevar un estilo de vida con la mayor calidad posible– INDIBA:
+                Método que utiliza el sistema de transferencia eléctrica capacitiva resistiva al aplicar corrientes
+                eléctricas para el tratamiento de lesiones musculo esqueléticas. También tratamos los hipopresivos con
+                esta técnica. – OTROS SERVICIOS: Vendajes funcionales y neuromusculares, punción seca, crioterapia,
+                termoterapia, electroterapia (TENS).
+                <br />
+              </div>
+            </div>
+            <div className="col-span-12 md:col-span-7 sm:col-span-12 "></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 export default Physiotherapy
 
