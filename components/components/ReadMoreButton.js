fbb198964a0591a93b1aaf10ee7ea94ab1852b9d
Nicholas Klave <nicholas.klave716@gmail.com>
2021-10-11 07:41:59 +0200

    Add ReadMoreButton component.

---
+++ components/components/ReadMoreButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// styles
+import styles from 'components/components/ReadMoreButton.module.scss'
+
+const ReadMoreButton = () => {
+  return <p className={styles.readMore}>[leer mas…]</p>
+}
+
+export default ReadMoreButton
