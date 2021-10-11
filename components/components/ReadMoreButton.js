eb675ed07c2a42f9d2d7f6126d6c7b9a7ff885e4
Nicholas Klave <nicholas.klave716@gmail.com>
2021-10-11 10:22:51 +0200

    Update ReadMoreButton component.

--- components/components/ReadMoreButton.js
+++ components/components/ReadMoreButton.js
@@ -3,8 +3,24 @@ import React from 'react'
 // styles
 import styles from 'components/components/ReadMoreButton.module.scss'
 
-const ReadMoreButton = () => {
-  return <p className={styles.readMore}>[leer mas…]</p>
+const ReadMoreButton = props => {
+  const { currentState, onClick } = props
+  const handleOnClick = () => {
+    if (currentState === 'more') {
+      onClick('less')
+    } else {
+      onClick('more')
+    }
+  }
+  return (
+    <p
+      onClick={() => handleOnClick()}
+      className={styles.readMore + ' ' + (currentState === 'less' ? ' ' : styles.expand)}
+    >
+      {/* currentState: more && less */}
+      {currentState === 'less' ? '[leer mas…]' : '[Leer menos...]'}
+    </p>
+  )
 }
 
 export default ReadMoreButton
