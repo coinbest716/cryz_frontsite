19ff5d6937965f0e629ee6ba1f628bf4b0514ed6
Nicholas Klave <nicholas.klave716@gmail.com>
2021-10-13 11:05:17 +0200

    Update BuyOneToOne page.

--- pages/buy-one-to-one.js
+++ pages/buy-one-to-one.js
@@ -2,11 +2,48 @@ import React from 'react'
 
 import PrimaryLayout from 'components/Layout/PrimaryLayout'
 
+// custom component
+import BackButton from 'components/components/BackButton'
+import CircularMark from 'components/components/CircularMark'
+import BuyCard from 'components/components/BuyCard'
+
+// json data
+import BuySessionData from 'assets/data/BuySessionData.json'
+
 // styles
+import globalStyles from 'styles/GlobalStyle.module.scss'
 import styles from 'pages/buy-one-to-one.module.scss'
 
 const BuyOneToOne = () => {
-  return <div>Buy One to One Streaming</div>
+  return (
+    <div className={styles.container}>
+      <div className={globalStyles.container}>
+        <div className={styles.backButtonArea}>
+          <BackButton />
+        </div>
+        <div className={'grid grid-cols-12 gap-4'}>
+          <div className={'col-span-6'}>
+            <div className={styles.title}>Bonos y Sesiones</div>
+            <div className={styles.divider} />
+            <div className={styles.text}>
+              Nuestros bonos solo pueden ser utilizados por una persona al mismo tiempo, si quieres mas información
+              puedes contactarnos sin compromiso
+            </div>
+          </div>
+          <div className={'col-span-6 flex justify-end z-10'}>
+            <CircularMark />
+          </div>
+        </div>
+        <div className={'mt-5 mb-10 grid grid-cols-12 gap-6'}>
+          {BuySessionData.map((item, index) => (
+            <div className={'col-span-4'} key={index}>
+              <BuyCard data={item} index={index} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default BuyOneToOne
