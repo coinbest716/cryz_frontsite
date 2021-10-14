60930b156989a14a464f2d9b84f48092e00de641
luming63 <laomingri@gmail.com>
2021-10-14 15:11:00 +0300

    added: show toast when click buy button

--- pages/_app.js
+++ pages/_app.js
@@ -5,10 +5,17 @@ import store from 'store.js'
 
 import 'styles/style.scss'
 
+import { Toaster } from 'react-hot-toast'
+
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || (page => page)
 
-  return <Provider store={store}>{getLayout(<Component {...pageProps} />)}</Provider>
+  return (
+    <>
+      <Provider store={store}>{getLayout(<Component {...pageProps} />)}</Provider>
+      <Toaster position="top-right" reverseOrder={false} />
+    </>
+  )
 }
 
 export default MyApp
