135f2b8430fed2fbc75900a9e51432d97865f09e
luming63 <laomingri@gmail.com>
2021-10-16 19:41:00 +0300

    added: previous button in billing Page

---
+++ components/components/purchase/PreviousButton.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+import backGrayIcon from 'public/images/arrow-left-gray.svg'
+import styles from './PreviousButton.module.scss'
+
+const PreviousButton = props => {
+  const { handleChangePrevious } = props
+
+  return (
+    <button className="flex justify-between items-center hover:bg-gray-300 px-2 py-1" onClick={handleChangePrevious}>
+      <Image src={backGrayIcon} alt="" width={15} height={15} />
+      <p className={styles.back}>Volver a Información</p>
+    </button>
+  )
+}
+
+export default PreviousButton
