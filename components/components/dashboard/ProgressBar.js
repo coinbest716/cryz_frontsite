0246d2c6e194e2041fcf76d54ce0ee49cd6f2bdb
luming63 <laomingri@gmail.com>
2021-10-20 19:16:47 +0300

    added: progress bar component

---
+++ components/components/dashboard/ProgressBar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './ProgressBar.module.scss'
+
+const ProgressBar = ({ percentage, label, type }) => {
+  return (
+    <div>
+      <div className="flex justify-between">
+        <div className={styles.label}>{label}</div>
+        <div className={styles.label}>{percentage}%</div>
+      </div>
+      <div className="h-2 w-full rounded bg-gray-100 mt-2">
+        <div
+          style={{ width: `${percentage}%` }}
+          className={`h-full rounded ${
+            type === 'women' ? 'bg-gray-200' : type === 'nutrition' ? 'bg-gray-400' : 'bg-gray-300'
+          }`}
+        ></div>
+      </div>
+    </div>
+  )
+}
+
+export default ProgressBar
