3cef664c15b032ca7fa99d9f0b444858835863cf
Nicholas Klave <nicholas.klave716@gmail.com>
2021-10-21 11:15:41 +0200

    Add buttons in OrderDetail page.

--- pages/dashboard/shopping/order-detail/index.js
+++ pages/dashboard/shopping/order-detail/index.js
@@ -4,12 +4,16 @@ import SecondaryLayout from 'components/Layout/SecondaryLayout'
 // custom components
 import Profile from 'components/components/dashboard/Profile'
 import NotificationButton from 'components/components/dashboard/NotificationButton'
+import CommonButton from 'components/components/dashboard/CommonButton'
 
 // styles
 import globalStyles from 'styles/GlobalStyle.module.scss'
 import styles from 'pages/dashboard/shopping/order-detail/OrderDetail.module.scss'
 
 const OrderDetail = () => {
+  const handleClickButton = () => {
+    console.log('clicked button')
+  }
   return (
     <div className={globalStyles.dashContainer}>
       <div className={'w-full flex flex-wrap justify-between items-center'}>
@@ -21,6 +25,14 @@ const OrderDetail = () => {
           <div>
             <Profile />
           </div>
+        </div>
+      </div>
+      <div className={'w-full flex justify-start mt-11 mb-8'}>
+        <div className={'mr-4'}>
+          <CommonButton label={'Pagar bono'} bgColor={'#BD5B54'} handleClickButton={() => handleClickButton()} />
+        </div>
+        <div>
+          <CommonButton label={'Comprar bono'} bgColor={'#818E8E'} handleClickButton={() => handleClickButton()} />
         </div>
       </div>
     </div>
