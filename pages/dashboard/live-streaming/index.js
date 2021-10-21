4606fdf7066d63598afb29605e53a37e84cbd704
luming63 <laomingri@gmail.com>
2021-10-21 09:35:22 +0300

    fixed: live-streaming Page

--- pages/dashboard/live-streaming/index.js
+++ pages/dashboard/live-streaming/index.js
@@ -1,10 +1,69 @@
 import SecondaryLayout from 'components/Layout/SecondaryLayout'
 import styles from './LiveStreaming.module.scss'
+import NotificationButton from 'components/components/dashboard/NotificationButton'
+import Profile from 'components/components/dashboard/Profile'
+import ReactPlayer from 'react-player'
 
 const LiveStreaming = () => {
+  const url = 'https://www.w3schools.com/html/mov_bbb.mp4'
+  const materials = [
+    {
+      url: '/images/card1.png',
+      label: 'Pesas de 2kg',
+      count: '2 unidades',
+    },
+    {
+      url: '/images/card1.png',
+      label: 'Esterilla fina',
+      count: '1 unidades',
+    },
+    {
+      url: '/images/card1.png',
+      label: 'Gomas elásticas',
+      count: '3 unidades',
+    },
+  ]
+
   return (
-    <div className={'flex flex-wrap ' + styles.container}>
-      <div className="h-full text-3xl">LiveStreaming</div>
+    <div className={'h-fll pt-12 pl-14 pr-8 h-screen ' + styles.container}>
+      <div className="grid grid-cols-12 gap-6">
+        <div className="col-span-12 md:col-span-8 sm:col-span-12 pt-4">
+          <div className={styles.title}>1 to 1 Streaming</div>
+          <div className="pt-14">
+            <ReactPlayer url={url} width="100%" height="100%" className={styles.reactPlayer} controls={true} />
+          </div>
+        </div>
+        <div className="col-span-12 md:col-span-4 sm:col-span-12">
+          <div className="flex justify-between items-center">
+            <NotificationButton />
+            <Profile />
+          </div>
+          <div className="rounded-xl bg-white py-4 px-16 pb-10 mt-10">
+            <div className={styles.material}>Material necesario</div>
+            <div className="pt-7">
+              {materials.map((item, index) => (
+                <div className="flex justify-start py-2" key={index}>
+                  <img
+                    src={item.url}
+                    alt=""
+                    style={{
+                      width: '56px',
+                      height: '56px',
+                      borderRadius: '10px',
+                      objectFit: 'cover',
+                      objectPosition: 'center',
+                    }}
+                  />
+                  <div className="pl-4 flex flex-col justify-around">
+                    <div className={styles.label}>{item.label}</div>
+                    <div className={styles.count}>{item.count}</div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
