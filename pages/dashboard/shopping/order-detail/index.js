335a7c8db3b0d8ff419de99f4fd711c11b9d3853
Nicholas Klave <nicholas.klave716@gmail.com>
2021-10-21 10:26:25 +0200

    Add header part in OrderDetail page.

--- pages/dashboard/shopping/order-detail/index.js
+++ pages/dashboard/shopping/order-detail/index.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import SecondaryLayout from 'components/Layout/SecondaryLayout'
 
+// custom components
+import Profile from 'components/components/dashboard/Profile'
+import NotificationButton from 'components/components/dashboard/NotificationButton'
+
 // styles
 import globalStyles from 'styles/GlobalStyle.module.scss'
 import styles from 'pages/dashboard/shopping/order-detail/OrderDetail.module.scss'
 
 const OrderDetail = () => {
-  return <div className={globalStyles.dashContainer}>OJO ICON - DETALLE PEDIDO (ORDER DETAIL)</div>
+  return (
+    <div className={globalStyles.dashContainer}>
+      <div className={'w-full flex flex-wrap justify-between items-center'}>
+        <div className={globalStyles.dashTitle}>Bono 10 sesiones Preparación parto</div>
+        <div className={'flex justify-end'}>
+          <div className={'mr-8'}>
+            <NotificationButton />
+          </div>
+          <div>
+            <Profile />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default OrderDetail
