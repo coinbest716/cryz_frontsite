99540e9dee6717a0de5d9f2c29d5bb056fc3776a
Nicholas Klave <nicholas.klave716@gmail.com>
2021-10-22 10:37:12 +0200

    Remove status icon from MessageCard01 component.

--- components/components/dashboard/MessageCard01.js
+++ components/components/dashboard/MessageCard01.js
@@ -6,9 +6,6 @@ import Image from 'next/image'
 // styles
 import styles from 'components/components/dashboard/MessageCard01.module.scss'
 
-// images
-import SeenIcon from 'assets/images/seen.svg'
-
 const MessageCard01 = props => {
   const { message } = props
   return (
@@ -16,7 +13,6 @@ const MessageCard01 = props => {
       <div className={styles.content}>{message.content}</div>
       <div className={'mt-4 flex justify-end items-center'}>
         <div className={styles.time}>{message.time}</div>
-        {message.state === 2 ? <Image src={SeenIcon} alt={''} width={29} height={24} /> : ''}
       </div>
       <div className={styles.triangle}></div>
     </div>
