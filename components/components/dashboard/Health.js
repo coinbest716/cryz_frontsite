7c2b8542519aba29220a5add0429f128b2b3edf5
luming63 <laomingri@gmail.com>
2021-10-22 10:03:51 +0300

    added: Health component

---
+++ components/components/dashboard/Health.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// next components
+import Image from 'next/image'
+
+// images
+import User from 'assets/images/team-member-01.png'
+
+// styles
+import styles from './Health.module.scss'
+
+const Health = () => {
+  return <div className={'px-24 py-12 ' + styles.container}>Health</div>
+}
+
+export default Health
