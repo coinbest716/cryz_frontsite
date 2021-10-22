2a9fa6d9cf995e846453663d818940e64b91e4ef
luming63 <laomingri@gmail.com>
2021-10-22 10:04:14 +0300

    added: Personal component for Dashboard profile page

---
+++ components/components/dashboard/Personal.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// next components
+import Image from 'next/image'
+
+// images
+import User from 'assets/images/team-member-01.png'
+
+// styles
+import styles from './Personal.module.scss'
+
+const Personal = () => {
+  return (
+    <div className="w-full h-full ">
+      <div className={'px-24 py-12 ' + styles.container}>Personal</div>
+      button
+    </div>
+  )
+}
+
+export default Personal
