179e9705dc4d536d3a29f8b872fd37db71ec56d7
luming63 <laomingri@gmail.com>
2021-10-27 02:45:59 +0300

    added: @fullCalendar component from noe_module

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,10 @@ import 'styles/style.scss'
 
 import { Toaster } from 'react-hot-toast'
 
+import '@fullcalendar/daygrid/main.css'
+import '@fullcalendar/common/main.css'
+import '@fullcalendar/timegrid/main.css'
+
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || (page => page)
 
--- pages/dashboard/calendar/index.js
+++ pages/dashboard/calendar/index.js
@@ -1,10 +1,30 @@
 import SecondaryLayout from 'components/Layout/SecondaryLayout'
 import styles from './calendar.module.scss'
+import FullCalendar, { formatDate } from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
 
 const Calendar = () => {
   return (
     <div className={'flex flex-wrap ' + styles.container}>
-      <div className="h-full text-3xl">Calendar</div>
+      <div className="h-full text-3xl">
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin]}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay',
+          }}
+          initialView="dayGridMonth"
+          editable={true}
+          selectable={true}
+          selectMirror={true}
+          dayMaxEvents={true}
+          weekends={true}
+          // initialEvents={classData} // alternatively, use the `events` setting to fetch from a feed
+          // events={classData}
+        />
+      </div>
     </div>
   )
 }
