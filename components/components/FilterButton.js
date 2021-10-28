3f16bb0db1bd6ebac5eac29f43a3b5e7918756f4
luming63 <laomingri@gmail.com>
2021-10-28 08:17:08 +0300

    added: FilterButton component for classland Page

---
+++ components/components/FilterButton.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from './FilterButton.module.scss'
+import ArrowRightUpGray from 'assets/images/arrow-right-up.svg'
+import ArrowRightUpWhite from 'assets/images/arrow-left.svg'
+
+const FilterButton = props => {
+  const { filter, onClick } = props
+
+  return (
+    <>
+      {filter?.active ? (
+        <button className={styles.activeButton + ' flex justify-between'} onClick={() => onClick(filter.id)}>
+          <p className={'mr-4 ' + styles.label}>{filter?.label}</p>
+          <Image src={ArrowRightUpWhite} alt="" width={23} height={22} />
+        </button>
+      ) : (
+        <button className={styles.deactiveButton + ' flex justify-between'} onClick={() => onClick(filter.id)}>
+          <p className={'mr-4 ' + styles.label}>{filter?.label}</p>
+          <Image src={ArrowRightUpGray} alt="" width={23} height={22} />
+        </button>
+      )}
+    </>
+  )
+}
+
+export default FilterButton
