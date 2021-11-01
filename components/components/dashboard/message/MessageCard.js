ccc2b015b4977b6b795e8f4d96b766d640bf7e27
Nicholas Klave <nicholas.klave716@gmail.com>
2021-11-01 19:34:01 +0100

    Fix some stuff in Message

--- components/components/dashboard/message/MessageCard.js
+++ components/components/dashboard/message/MessageCard.js
@@ -7,7 +7,7 @@ import Image from 'next/image'
 import styles from './MessageCard.module.scss'
 
 // images
-import CheckIcon from 'assets/images/check-white.svg'
+import CheckIcon from 'assets/images/check.svg'
 
 const MessageCard = () => {
   return (
