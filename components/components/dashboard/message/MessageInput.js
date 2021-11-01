a137915eeb1efa40bb20fcd34505d14248f13884
Nicholas Klave <nicholas.klave716@gmail.com>
2021-11-01 07:50:49 +0100

    Update MessageInput component.

--- components/components/dashboard/message/MessageInput.js
+++ components/components/dashboard/message/MessageInput.js
@@ -9,6 +9,10 @@ import styles from 'components/components/dashboard/message/MessageInput.module.
 // images
 import SendIcon from 'assets/images/send.svg'
 
+import SmileIcon from 'assets/images/smile.svg'
+import AttachIcon from 'assets/images/attach.svg'
+import SendWhiteIcon from 'assets/images/send-white.svg'
+
 const MessageInput = props => {
   const { message, sendMessage } = props
   const [content, setContent] = React.useState('')
@@ -23,24 +27,44 @@ const MessageInput = props => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.inputArea}>
-        <input
-          type={'text'}
-          value={content}
-          placeholder={'Escribe tu mensaje'}
-          onChange={event => handleSetContent(event)}
-          className={
-            'appearance-none bg-white border-none w-full leading-tight focus:outline-none focus ' + styles.input
-          }
-        />
+      {/* input part */}
+      <div className={'w-full flex bg-white'}>
+        <div className={styles.inputArea}>
+          <input
+            type={'text'}
+            value={content}
+            placeholder={'Escribe tu mensaje'}
+            onChange={event => handleSetContent(event)}
+            className={
+              'appearance-none bg-white border-none w-full leading-tight focus:outline-none focus ' + styles.input
+            }
+          />
+        </div>
+        <div className={styles.iconArea}>
+          <button
+            className="duration-200 hover:bg-gray-300 rounded-full p-3 flex justify-center items-center"
+            onClick={() => sendMessage(content, 'text')}
+          >
+            <Image src={SendIcon} alt={''} width={18} height={18} />
+          </button>
+        </div>
       </div>
-      <div className={styles.iconArea}>
-        <button
-          className="duration-200 hover:bg-gray-300 rounded-full p-3 flex justify-center items-center"
-          onClick={() => sendMessage(content, 'text')}
-        >
-          <Image src={SendIcon} alt={''} width={18} height={18} />
+      {/* icon part */}
+      <div className={'w-full flex justify-end items-center mt-4'}>
+        <button className="duration-200 hover:bg-gray-300 rounded-full p-3 flex justify-center items-center">
+          <Image src={SmileIcon} alt={''} width={21} height={21} />
         </button>
+        <button className="duration-200 hover:bg-gray-300 rounded-full p-3 flex justify-center items-center">
+          <Image src={AttachIcon} alt={''} width={21} height={21} />
+        </button>
+        <div className={'ml-4'}>
+          <button className={styles.enterButton}>
+            <div className={'flex items-center mr-3'}>
+              <Image src={SendWhiteIcon} alt={''} width={16} height={16} />
+            </div>
+            Enviar
+          </button>
+        </div>
       </div>
     </div>
   )
