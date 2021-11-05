17839f66e05f90a566d70451043c6c517631d688
Nicholas Klave <nicholas.klave716@gmail.com>
2021-11-05 19:20:25 +0100

    Add cookies page.

---
+++ pages/docs/cookies.js
@@ -0,0 +1,340 @@
+import PrimaryLayout from 'components/Layout/PrimaryLayout'
+
+// styles
+import globalStyles from 'styles/GlobalStyles.module.scss'
+import styles from 'pages/docs/index.module.scss'
+
+const Cookies = () => {
+  return (
+    <div className="flex justify-center">
+      <div className={globalStyles.container}>
+        <div className={styles.title}>Política de cookies</div>
+        <div className={styles.divider} />
+        {/* paragraph */}
+        <div className={styles.text}>Crody Salud S.L. informa acerca del uso de las cookies en su página web:</div>
+        {/* paragraph */}
+        <div className={styles.text}>https://crysdyazandco.com/</div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <strong>¿Qué son las cookies?</strong>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          Las cookies son archivos que se pueden descargar en su equipo a través de las páginas web. Son herramientas
+          que tienen un papel esencial para la prestación de numerosos servicios de la sociedad de la información. Entre
+          otros, permiten a una página web almacenar y recuperar información sobre los hábitos de navegación de un
+          usuario o de su equipo y, dependiendo de la información obtenida, se pueden utilizar para reconocer al usuario
+          y mejorar el servicio ofrecido.
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <strong>Tipos de cookies</strong>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          Según quien sea la entidad que gestione el dominio desde donde se envían las cookies y trate los datos que se
+          obtengan se pueden distinguir dos tipos:
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-4'}>
+            Cookies propias: Aquéllas que se envían al equipo terminal del usuario desde un equipo o dominio gestionado
+            por el propio editor y desde el que se presta el servicio solicitado por el usuario.
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-4'}>
+            Cookies de terceros: Aquéllas que se envían al equipo terminal del usuario desde un equipo o dominio que no
+            es gestionado por el editor, sino por otra entidad que trata los datos obtenidos través de las cookies.
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          En el caso de que las cookies sean instaladas desde un equipo o dominio gestionado por el propio editor pero
+          la información que se recoja mediante éstas sea gestionada por un tercero, no pueden ser consideradas como
+          cookies propias.
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          Existe también una segunda clasificación según el plazo de tiempo que permanecen almacenadas en el navegador
+          del cliente, pudiendo tratarse de:
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-4'}>
+            Cookies de sesión: Diseñadas para recabar y almacenar datos mientras el usuario accede a una página web. Se
+            suelen emplear para almacenar información que solo interesa conservar para la prestación del servicio
+            solicitado por el usuario en una sola ocasión (p.e. una lista de productos adquiridos).
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-4'}>
+            Cookies persistentes: Los datos siguen almacenados en el terminal y pueden ser accedidos y tratados durante
+            un periodo definido por el responsable de la cookie, y que puede ir de unos minutos a varios años.
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          En el caso de que las cookies sean instaladas desde un equipo o dominio gestionado por el propio editor pero
+          la información que se recoja mediante éstas sea gestionada por un tercero, no pueden ser consideradas como
+          cookies propias.
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          Existe también una segunda clasificación según el plazo de tiempo que permanecen almacenadas en el navegador
+          del cliente, pudiendo tratarse de:
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-4'}>
+            Cookies de sesión: Diseñadas para recabar y almacenar datos mientras el usuario accede a una página web. Se
+            suelen emplear para almacenar información que solo interesa conservar para la prestación del servicio
+            solicitado por el usuario en una sola ocasión (p.e. una lista de productos adquiridos).
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-4'}>
+            Cookies persistentes: Los datos siguen almacenados en el terminal y pueden ser accedidos y tratados durante
+            un periodo definido por el responsable de la cookie, y que puede ir de unos minutos a varios años.
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <strong>
+            Por último, existe otra clasificación con seis tipos de cookies según la finalidad para la que se traten los
+            datos obtenidos:
+          </strong>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-4'}>
+            Cookies técnicas: Aquellas que permiten al usuario la navegación a través de una página web, plataforma o
+            aplicación y la utilización de las diferentes opciones o servicios que en ella existan como, por ejemplo,
+            controlar el tráfico y la comunicación de datos, identificar la sesión, acceder a partes de acceso
+            restringido, recordar los elementos que integran un pedido, realizar el proceso de compra de un pedido,
+            realizar la solicitud de inscripción o participación en un evento, utilizar elementos de seguridad durante
+            la navegación, almacenar contenidos para la difusión de vídeos o sonido o compartir contenidos a través de
+            redes sociales.
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-4'}>
+            Cookies de personalización: Permiten al usuario acceder al servicio con algunas características de carácter
+            general predefinidas en función de una serie de criterios en el terminal del usuario como por ejemplo serian
+            el idioma, el tipo de navegador a través del cual accede al servicio, la configuración regional desde donde
+            accede al servicio, etc.
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-4'}>
+            Cookies de análisis: Permiten al responsable de las mismas, el seguimiento y análisis del comportamiento de
+            los usuarios de los sitios web a los que están vinculadas. La información recogida mediante este tipo de
+            cookies se utiliza en la medición de la actividad de los sitios web, aplicación o plataforma y para la
+            elaboración de perfiles de navegación de los usuarios de dichos sitios, aplicaciones y plataformas, con el
+            fin de introducir mejoras en función del análisis de los datos de uso que hacen los usuarios del servicio.
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-4'}>
+            Cookies publicitarias: Permiten la gestión, de la forma más eficaz posible, de los espacios publicitarios.
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-4'}>
+            Cookies de publicidad comportamental: Almacenan información del comportamiento de los usuarios obtenida a
+            través de la observación continuada de sus hábitos de navegación, lo que permite desarrollar un perfil
+            específico para mostrar publicidad en función del mismo.
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-4'}>
+            Cookies de redes sociales externas: Se utilizan para que los visitantes puedan interactuar con el contenido
+            de diferentes plataformas sociales (facebook, youtube, twitter, linkedIn, etc..) y que se generen únicamente
+            para los usuarios de dichas redes sociales. Las condiciones de utilización de estas cookies y la información
+            recopilada se regula por la política de privacidad de la plataforma social correspondiente.
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <strong>Desactivación y eliminación de cookies</strong>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          Tienes la opción de permitir, bloquear o eliminar las cookies instaladas en tu equipo mediante la
+          configuración de las opciones del navegador instalado en su equipo. Al desactivar cookies, algunos de los
+          servicios disponibles podrían dejar de estar operativos. La forma de deshabilitar las cookies es diferente
+          para cada navegador, pero normalmente puede hacerse desde el menú Herramientas u Opciones. También puede
+          consultarse el menú de Ayuda del navegador dónde puedes encontrar instrucciones. El usuario podrá en cualquier
+          momento elegir qué cookies quiere que funcionen en este sitio web.
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          Puede usted permitir, bloquear o eliminar las cookies instaladas en su equipo mediante la configuración de las
+          opciones del navegador instalado en su ordenador:
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-4'}>
+            Microsoft Internet Explorer o Microsoft Edge:&nbsp;
+            <a href="http://windows.microsoft.com/es-es/windows-vista/Block-or-allow-cookies">
+              http://windows.microsoft.com/es-es/windows-vista/Block-or-allow-cookies
+            </a>
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-4'}>
+            Mozilla Firefox:&nbsp;
+            <a href="http://support.mozilla.org/es/kb/impedir-que-los-sitios-web-guarden-sus-preferencia">
+              http://support.mozilla.org/es/kb/impedir-que-los-sitios-web-guarden-sus-preferencia
+            </a>
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-4'}>
+            Chrome:&nbsp;
+            <a href="https://support.google.com/accounts/answer/61416?hl=es">
+              https://support.google.com/accounts/answer/61416?hl=es
+            </a>
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-4'}>
+            Safari:&nbsp;
+            <a href="http://safari.helpmax.net/es/privacidad-y-seguridad/como-gestionar-las-cookies/">
+              http://safari.helpmax.net/es/privacidad-y-seguridad/como-gestionar-las-cookies/
+            </a>
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-4'}>
+            Opera:&nbsp;
+            <a href="http://help.opera.com/Linux/10.60/es-ES/cookies.html">
+              http://help.opera.com/Linux/10.60/es-ES/cookies.html
+            </a>
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          Además, también puede gestionar el almacén de cookies en su navegador a través de herramientas como las
+          siguientes:
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-4'}>
+            Ghostery:&nbsp;
+            <a href="www.ghostery.com">www.ghostery.com</a>
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-4'}>
+            Your Online Choices:&nbsp;
+            <a href="www.youronlinechoices.com/es/">www.youronlinechoices.com/es/</a>
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          Cookies utilizadas en&nbsp;<a href="https://crysdyazandco.com/">https://crysdyazandco.com/</a>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          A continuación se identifican las cookies que están siendo utilizadas en este portal así como su tipología y
+          función:
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-4'}>_utma</li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-8'} style={{ listStyleType: 'circle' }}>
+            Duración: 2 años a partir de su instalación o actualización.
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-8'} style={{ listStyleType: 'circle' }}>
+            Descripción: Recopila información anónima sobre la navegación de los usuarios por el sitio web con el fin de
+            conocer el origen de las visitas y otros datos estadísticos. Esta cookie normalmente se almacena en el
+            navegador en la primera visita a nuestro sitio web. Si la cookie ha sido borrada a través del navegador y
+            posteriormente vuelve a visitar nuestro sitioweb, una nueva cookie del tipo _utma se escribe con un ID único
+            diferente. Esta cookie se utiliza para determinar visitantes únicos a nuestro sitio web y se actualiza con
+            cada vista de página. Adicionalmente, esta cookie proporciona un identificador único que Google Analytics
+            utiliza para asegurar la validez y la accesibilidad de la cookie, así como una medida de seguridad extra.
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-8'} style={{ listStyleType: 'circle' }}>
+            Tipo: Tercero
+          </li>
+        </div>
+        <br />
+        {/* paragraph */}
+        <div className={styles.text}>
+          <li className={'ml-8'} style={{ listStyleType: 'circle' }}>
+            Finalidad: Analítica
+          </li>
+        </div>
+        <br />
+
+        <div className={'mb-10'} />
+      </div>
+    </div>
+  )
+}
+export default Cookies
+
+Cookies.getLayout = function getLayout(page) {
+  return <PrimaryLayout>{page}</PrimaryLayout>
+}
