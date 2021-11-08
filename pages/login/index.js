3be4d2a4b08543985ad6637bd1b3e4c569155c7a
luming63 <laomingri@gmail.com>
2021-11-08 14:00:07 +0300

    fixed: authChallenge with new password

--- pages/login/index.js
+++ pages/login/index.js
@@ -119,6 +119,22 @@ const Login = () => {
     // router.push('/dashboard')
   }
 
+  const handleUpdatePassword = async () => {
+    setProgressStatus(true)
+    await Auth.completeNewPassword(authUser, password)
+      .then(response => {
+        setProgressStatus(false)
+        setAuthUser(response)
+        setAuthChallenge('')
+        toast.success('Successfully update password')
+        router.push('/dashboard')
+      })
+      .catch(error => {
+        setProgressStatus(false)
+        toast.error(error.message)
+      })
+  }
+
   return (
     <div className="relative">
       <div className="w-full h-screen flex grid grid-cols-12">
@@ -133,95 +149,123 @@ const Login = () => {
             </div>
           </div>
         </div>
-        <div className={'w-full col-span-6 flex flex-wrap justify-center items-center relative ' + styles.grayArea}>
-          <div className={styles.closeButton}>
-            <button
-              className="duration-200 hover:bg-gray-300 rounded-full p-3 flex justify-center items-center"
-              onClick={() => router.push('/')}
-            >
-              <Image src={CloseIcon} alt="" width={19} height={20} />
-            </button>
+        {authChallenge === '' ? (
+          <div className={'w-full col-span-6 flex flex-wrap justify-center items-center relative ' + styles.grayArea}>
+            <div className={styles.closeButton}>
+              <button
+                className="duration-200 hover:bg-gray-300 rounded-full p-3 flex justify-center items-center"
+                onClick={() => router.push('/')}
+              >
+                <Image src={CloseIcon} alt="" width={19} height={20} />
+              </button>
+            </div>
+            <div className={styles.grayAreaContent}>
+              <div className="w-full flex justify-start">
+                <button className={styles.loginButton} onClick={() => router.push('/login')}>
+                  LOGIN
+                </button>
+                <button className={styles.registerButton} onClick={() => router.push('/register')}>
+                  REGISTRO
+                </button>
+              </div>
+              {/* email input */}
+              <div className={'w-full mt-9 ' + styles.inputArea}>
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className="w-full h-full border border-white rounded bg-transparent py-1 px-2 text-white"
+                  value={email}
+                  onChange={handleChangeEmail}
+                />
+              </div>
+              {/* password input */}
+              <div className={'w-full relative flex items-center mt-5 ' + styles.inputArea}>
+                <input
+                  type={showPass === true ? 'text' : 'password'}
+                  placeholder="Contraseña"
+                  className="w-full h-full border border-white rounded bg-transparent py-1 pl-2 text-white pr-10"
+                  value={password}
+                  onChange={handleChangePassword}
+                />
+                <div className="absolute right-3 cursor-pointer">
+                  {showPass === true ? (
+                    <Image src={EyeIcon} alt="" width={17} height={17} onClick={() => handleSetShowPass(false)} />
+                  ) : (
+                    <Image src={EyeCrossIcon} alt="" width={17} height={17} onClick={() => handleSetShowPass(true)} />
+                  )}
+                </div>
+              </div>
+              {/* login button part */}
+              <div className="mt-5 flex justify-between items-center">
+                <div>
+                  <label className="inline-flex items-center cursor-pointer">
+                    <input
+                      type="checkbox"
+                      className={'form-checkbox cursor-pointer ' + styles.checkbox}
+                      onChange={() => handleSetRememberMe()}
+                      checked={rememberMe}
+                    />
+                    <span className="ml-2" onChange={() => handleSetRememberMe()}>
+                      Recuérdame
+                    </span>
+                  </label>
+                </div>
+                <div>
+                  <button className={styles.enterButton} onClick={handleClickLogin}>
+                    Entrar
+                  </button>
+                </div>
+              </div>
+              {/* forgot password part */}
+              <div
+                className={'mt-3.5 flex justify-end items-center ' + styles.forgotPass}
+                onClick={() => router.push('forgot-password')}
+              >
+                Olvidaste contraseña
+              </div>
+              {/* ------------------- or ------------------- part */}
+              <div className="mt-11 flex justify-between items-center">
+                <div className={styles.line} />
+                <div className={styles.orText}>or</div>
+                <div className={styles.line} />
+              </div>
+              {/* facebook button */}
+              <div className="mt-11">
+                <CommonButton handleClick={handleClickFacebook} label={'LOGIN CON FACEBOOK'} type={'facebook'} />
+              </div>
+              {/* google button */}
+              <div className="mt-6">
+                <CommonButton handleClick={handleClickGoogle} label={'LOGIN CON GOOGLE'} type={'google'} />
+              </div>
+            </div>
           </div>
-          <div className={styles.grayAreaContent}>
-            <div className="w-full flex justify-start">
-              <button className={styles.loginButton} onClick={() => router.push('/login')}>
-                LOGIN
-              </button>
-              <button className={styles.registerButton} onClick={() => router.push('/register')}>
-                REGISTRO
-              </button>
-            </div>
-            {/* email input */}
-            <div className={'w-full mt-9 ' + styles.inputArea}>
-              <input
-                type="text"
-                placeholder="Email"
-                className="w-full h-full border border-white rounded bg-transparent py-1 px-2 text-white"
-                value={email}
-                onChange={handleChangeEmail}
-              />
-            </div>
-            {/* password input */}
-            <div className={'w-full relative flex items-center mt-5 ' + styles.inputArea}>
-              <input
-                type={showPass === true ? 'text' : 'password'}
-                placeholder="Contraseña"
-                className="w-full h-full border border-white rounded bg-transparent py-1 pl-2 text-white pr-10"
-                value={password}
-                onChange={handleChangePassword}
-              />
-              <div className="absolute right-3 cursor-pointer">
-                {showPass === true ? (
-                  <Image src={EyeIcon} alt="" width={17} height={17} onClick={() => handleSetShowPass(false)} />
-                ) : (
-                  <Image src={EyeCrossIcon} alt="" width={17} height={17} onClick={() => handleSetShowPass(true)} />
-                )}
+        ) : (
+          <div className={'w-full col-span-6 flex flex-wrap flex justify-center items-center ' + styles.grayArea}>
+            <div>
+              <div className={'w-full relative flex items-center mt-5 ' + styles.inputArea}>
+                <input
+                  type={showPass === true ? 'text' : 'password'}
+                  placeholder="Contraseña"
+                  className="w-full h-full border border-white rounded bg-transparent py-1 pl-2 text-white pr-10"
+                  value={password}
+                  onChange={handleChangePassword}
+                />
+                <div className="absolute right-3 cursor-pointer">
+                  {showPass === true ? (
+                    <Image src={EyeIcon} alt="" width={17} height={17} onClick={() => handleSetShowPass(false)} />
+                  ) : (
+                    <Image src={EyeCrossIcon} alt="" width={17} height={17} onClick={() => handleSetShowPass(true)} />
+                  )}
+                </div>
               </div>
-            </div>
-            {/* login button part */}
-            <div className="mt-5 flex justify-between items-center">
-              <div>
-                <label className="inline-flex items-center cursor-pointer">
-                  <input
-                    type="checkbox"
-                    className={'form-checkbox cursor-pointer ' + styles.checkbox}
-                    onChange={() => handleSetRememberMe()}
-                    checked={rememberMe}
-                  />
-                  <span className="ml-2" onChange={() => handleSetRememberMe()}>
-                    Recuérdame
-                  </span>
-                </label>
-              </div>
-              <div>
-                <button className={styles.enterButton} onClick={handleClickLogin}>
+              <div className={'mt-10 flex justify-end'}>
+                <button className={styles.enterButton} onClick={handleUpdatePassword}>
                   Entrar
                 </button>
               </div>
             </div>
-            {/* forgot password part */}
-            <div
-              className={'mt-3.5 flex justify-end items-center ' + styles.forgotPass}
-              onClick={() => router.push('forgot-password')}
-            >
-              Olvidaste contraseña
-            </div>
-            {/* ------------------- or ------------------- part */}
-            <div className="mt-11 flex justify-between items-center">
-              <div className={styles.line} />
-              <div className={styles.orText}>or</div>
-              <div className={styles.line} />
-            </div>
-            {/* facebook button */}
-            <div className="mt-11">
-              <CommonButton handleClick={handleClickFacebook} label={'LOGIN CON FACEBOOK'} type={'facebook'} />
-            </div>
-            {/* google button */}
-            <div className="mt-6">
-              <CommonButton handleClick={handleClickGoogle} label={'LOGIN CON GOOGLE'} type={'google'} />
-            </div>
           </div>
-        </div>
+        )}
       </div>
       {progressStatus && (
         <div className={globalStyles.loading}>
