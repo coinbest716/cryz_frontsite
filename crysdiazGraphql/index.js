82c0a52de29e05c7e26e34f27169e075cf74ac6a
Nicholas Klave <nicholas.klave716@gmail.com>
2021-11-19 07:51:56 +0100

    Add home graphql queries.

--- crysdiazGraphql/index.js
+++ crysdiazGraphql/index.js
@@ -1,8 +1,13 @@
-import { GetContactInfo, GetUser, CreateUser } from './crysdiaz.gql'
+import { GetMainImage, GetFeaturedServices } from './home.gql'
 import { GetClasslands, GetFaqs } from './classland.gql'
+import { GetContactInfo, GetUser, CreateUser } from './crysdiaz.gql'
 
 export default {
   queries: {
+    // home part
+    getMainImage: GetMainImage,
+    getFeaturedServices: GetFeaturedServices,
+    // classland part
     getClasslands: GetClasslands,
     getContactInfo: GetContactInfo,
     getFaqs: GetFaqs,
