28bb29df89815a2ebeb68ce09fd56b1b5be12221
Luis Morales <luismmorales@gmail.com>
2021-11-21 11:34:23 +0100

    added sentry for easier debuggin with the api

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 const path = require('path')
+const { withSentryConfig } = require('@sentry/nextjs');
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
 const withTM = require('next-transpile-modules')([
   '@fullcalendar/common',
   '@fullcalendar/interaction',
@@ -31,4 +45,4 @@ const appConfig = {
   },
 }
 
-module.exports = withGraphql(withTM(appConfig))
+module.exports = withSentryConfig(withGraphql(withTM(appConfig)), sentryWebpackPluginOptions)
