4dd8e230c74c179c2a98f2a4d10d20079b9ff56d
Nicholas Klave <nicholas.klave716@gmail.com>
2021-11-23 07:04:01 +0100

    Add female-health graphql queries.

--- pages/female-health/index.js
+++ pages/female-health/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 // redux
 import { useDispatch } from 'react-redux'
@@ -9,6 +9,10 @@ import MainSection from 'components/FemaleHealth/MainSection'
 import DisciplineSection from 'components/FemaleHealth/DisciplineSection'
 
 import styles from 'pages/female-health/FemaleHealth.module.scss'
+
+// graphql
+import { useLazyQuery } from '@apollo/client'
+import graphql from 'crysdiazGraphql'
 
 const FemaleHealth = props => {
   // loading part ###########################
@@ -30,9 +34,25 @@ const FemaleHealth = props => {
   // variables
   const { viewport } = props
 
+  const [femHealth, setFemHealth] = useState({})
+  const [getFemHealth, { data: femHealthData, loading: femHealthLoading, error: femHealthError }] = useLazyQuery(
+    graphql.queries.getFemHealth
+  )
+
+  // handlers
+  useEffect(() => {
+    getFemHealth()
+  }, [])
+
+  useEffect(() => {
+    if (!femHealthError && femHealthData && femHealthData.getFemHealth) {
+      setFemHealth(femHealthData.getFemHealth)
+    }
+  }, [femHealthLoading, femHealthData, femHealthError])
+
   return (
     <div className={styles.container}>
-      <MainSection />
+      {JSON.stringify(femHealth) !== '{}' ? <MainSection data={femHealth} /> : <></>}
       <div id="discipline" className={'w-full flex justify-center'}>
         <DisciplineSection viewport={viewport} />
       </div>
