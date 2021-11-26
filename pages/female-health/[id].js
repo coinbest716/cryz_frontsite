d483a16a27be85572c21318323d985ee7bd757d5
Nicholas Klave <nicholas.klave716@gmail.com>
2021-11-26 19:28:04 +0100

    Fix some issue in id page.

--- pages/female-health/[id].js
+++ pages/female-health/[id].js
@@ -149,9 +149,3 @@ export default Menopause
 Menopause.getLayout = function getLayout(page) {
   return <PrimaryLayout>{page}</PrimaryLayout>
 }
-
-export async function getServerSideProps(context) {
-  return {
-    props: {},
-  }
-}
