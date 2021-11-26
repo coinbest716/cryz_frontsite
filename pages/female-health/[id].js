a79800b0dfff752f60675908e4e7b45770e4e905
Nicholas Klave <nicholas.klave716@gmail.com>
2021-11-26 07:05:15 +0100

    Update female-health sub page with graphql data.

---
+++ pages/female-health/[id].js
@@ -0,0 +1,159 @@
+import React, { useEffect, useState } from 'react'
+
+// redux
+import { useDispatch } from 'react-redux'
+
+// custom component
+import PrimaryLayout from 'components/Layout/PrimaryLayout'
+import BackButton from 'components/components/BackButton'
+import ReadMoreButton from 'components/components/ReadMoreButton'
+import CircularMark from 'components/components/CircularMark'
+import CarouselFemaleHealth from 'components/FemaleHealth/CarouselFemaleHealth'
+import OutlineButton from 'components/components/OutlineButton'
+
+// styles
+import globalStyles from 'styles/GlobalStyles.module.scss'
+import styles from 'pages/female-health/PreparationForChildbirth.module.scss'
+
+// json data
+import ServerPhysiotherapy from 'assets/data/ServerPhysiotherapy'
+import { useRouter } from 'next/router'
+
+// graphql
+import { useLazyQuery } from '@apollo/client'
+import graphql from 'crysdiazGraphql'
+
+const Menopause = () => {
+  // loading part ###########################
+  const dispatch = useDispatch()
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+    return () => setIsMounted(false)
+  }, [])
+
+  useEffect(() => {
+    if (isMounted === true) {
+      dispatch({ type: 'set', isLoading: false })
+    }
+  }, [isMounted, dispatch])
+  // loading part end #######################
+
+  // variables
+  const router = useRouter()
+  const { pid } = router.query
+  const [sliderData, setSliderData] = useState([])
+  const [readMoreCurrentState, setReadMoreCurrentState] = useState('less')
+  const [disciplineID, setDisciplineID] = useState(1)
+
+  const [femHealthService, setFemHealthService] = useState({})
+  const [
+    getFemHealthService,
+    { data: femHealthServiceData, loading: femHealthServiceLoading, error: femHealthServiceError },
+  ] = useLazyQuery(graphql.queries.getFemHealthService)
+
+  // handlers
+  useEffect(() => {
+    setDisciplineID(Number(router.asPath.split('/')[2]))
+  }, [router.path])
+
+  useEffect(() => {
+    if (disciplineID !== null) {
+      getFemHealthService({ variables: { discipline_id: disciplineID } })
+    }
+  }, [disciplineID])
+
+  useEffect(() => {
+    if (!femHealthServiceError && femHealthServiceData && femHealthServiceData.getFemHealthService) {
+      setFemHealthService(femHealthServiceData.getFemHealthService)
+    }
+  }, [femHealthServiceLoading, femHealthServiceData, femHealthServiceError])
+
+  useEffect(() => {
+    setSliderData(ServerPhysiotherapy)
+  }, [])
+
+  const handleReadMore = state => {
+    setReadMoreCurrentState(state)
+  }
+
+  return (
+    <div className={'w-full flex flex-wrap justify-center'}>
+      {console.log(femHealthService)}
+      <div className={globalStyles.container}>
+        <div className={styles.backButtonArea}>
+          <BackButton />
+        </div>
+        <div className={'grid grid-cols-12 gap-4'} style={{ minHeight: '634px' }}>
+          <div className={'col-span-5 block'}>
+            <div className={styles.strokeTitle}>{femHealthService.title_one}</div>
+            <div className={styles.pinkTitle}>{femHealthService.title_two}</div>
+            <div className={styles.divider} />
+            <div className={'relative ' + styles.text + ' ' + (readMoreCurrentState === 'less' ? '' : styles.expand)}>
+              <div id="text" dangerouslySetInnerHTML={{ __html: femHealthService.text }}></div>
+              <ReadMoreButton currentState={readMoreCurrentState} onClick={state => handleReadMore(state)} />
+            </div>
+          </div>
+          <div className={'col-span-7 relative flex justify-end'}>
+            <div className={'absolute top-10 z-10'}>
+              <CircularMark />
+            </div>
+            <div className={'w-full h-full mt-20 pb-20'}>
+              {femHealthService?.carousel_image !== undefined ? (
+                <CarouselFemaleHealth sliderData={femHealthService?.carousel_image} />
+              ) : (
+                <></>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Button group part */}
+      <div className={'w-full pt-32'}>
+        <div className={'grid grid-cols-12 gap-0'}>
+          {femHealthService.stream_button ? (
+            <div className={'col-span-4 w-full flex'}>
+              <OutlineButton
+                title="Compra 1 to 1 en streaming"
+                link="/buy/buy-one-to-one"
+                onClick={link => router.push(link)}
+              />
+            </div>
+          ) : (
+            <></>
+          )}
+          {femHealthService.personal_button ? (
+            <div className={'col-span-4 w-full flex'}>
+              <OutlineButton title="Compra presenciales" link="/buy/buy-person" onClick={link => router.push(link)} />
+            </div>
+          ) : (
+            <></>
+          )}
+          {femHealthService.online_button ? (
+            <div className={'col-span-4 w-full flex'}>
+              <OutlineButton
+                title="Compra planes online"
+                link="/buy/buy-plans-online"
+                onClick={link => router.push(link)}
+              />
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Menopause
+
+Menopause.getLayout = function getLayout(page) {
+  return <PrimaryLayout>{page}</PrimaryLayout>
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: {},
+  }
+}
