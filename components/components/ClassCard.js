de779408caa8aba9c121f5b144d4b144d3d75033
luming63 <laomingri@gmail.com>
2021-11-29 17:11:45 +0100

    fixed: wc-28, open new tab

--- components/components/ClassCard.js
+++ components/components/ClassCard.js
@@ -7,24 +7,26 @@ const ClassCard = props => {
   const { data } = props
 
   return (
-    <div className={'cursor-pointer ' + styles.singleCard} onClick={() => router.push(data?.url)}>
-      <div className={styles.cardImageLayer}>
-        <Image
-          src={data?.image ? data?.image : 'https://via.placeholder.com/365x253?text=Placeholder'}
-          alt=""
-          layout="responsive"
-          width={365}
-          height={253}
-          className={styles.cardImage}
-        />
-        <div className={styles.cardPayment}>
-          <span className={styles.cardPaymentType}>{data?.price}€/mes</span>
+    <a target="_blank" href={data?.url} rel="noopener noreferrer">
+      <div className={'cursor-pointer ' + styles.singleCard}>
+        <div className={styles.cardImageLayer}>
+          <Image
+            src={data?.image ? data?.image : 'https://via.placeholder.com/365x253?text=Placeholder'}
+            alt=""
+            layout="responsive"
+            width={365}
+            height={253}
+            className={styles.cardImage}
+          />
+          <div className={styles.cardPayment}>
+            <span className={styles.cardPaymentType}>{data?.price}€/mes</span>
+          </div>
         </div>
+        <div className={styles.cardName}>{data?.name}</div>
+        <div className={styles.cardDescripton}>{data?.description}</div>
+        <div className={styles.cardDifficulty}>Dificultad {data?.difficulty}</div>
       </div>
-      <div className={styles.cardName}>{data?.name}</div>
-      <div className={styles.cardDescripton}>{data?.description}</div>
-      <div className={styles.cardDifficulty}>Dificultad {data?.difficulty}</div>
-    </div>
+    </a>
   )
 }
 
