b1036922a441ad4436afbf59e1da802f411210d4
luming63 <laomingri@gmail.com>
2021-12-13 15:37:39 +0100

    fixed: deploy error

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,8 @@ class MyDocument extends Document {
         <Head>
           <Fragment>
             <script type="text/javascript" src="https://cdn.iubenda.com/cons/iubenda_cons.js" async />
+            <script src="https://js.stripe.com/v2/"></script>
+            <script id="stripe-js" src="https://js.stripe.com/v3/" async></script>
           </Fragment>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Zilla+Slab:ital@1&display=swap" rel="stylesheet" />
@@ -18,8 +20,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Montserrat:400,400italic,700&display=swap"
             rel="stylesheet"
           />
-          <script src="https://js.stripe.com/v2/"></script>
-          <script id="stripe-js" src="https://js.stripe.com/v3/" async></script>
         </Head>
         <body>
           <Main />
