3a102efd0b555626d099e7ce7bc84837eff501dd
luming63 <laomingri@gmail.com>
2021-12-14 12:04:46 +0100

    fixed: conflict error

--- pages/purchase-login.js
+++ pages/purchase-login.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 
 // next components
+import Image from 'next/image'
 import Link from 'next/link'
 import router from 'next/router'
 
@@ -16,6 +17,10 @@ import ShoppingCart from 'components/components/purchaseLogin/ShoppingCart'
 import globalStyles from 'styles/GlobalStyles.module.scss'
 import styles from 'pages/purchase-login.module.scss'
 
+import EyeCrossIcon from 'assets/images/eye-cross-gray.svg'
+import EyeIcon from 'assets/images/eye-gray.svg'
+
+import { Auth } from 'aws-amplify'
 // json data
 import shoppingCartData from 'assets/data/ShoppingCartData'
 
@@ -38,13 +43,40 @@ const PurchaseLogin = () => {
 
   // variables
   const [cartData, setCartData] = useState([])
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [authChallenge, setAuthChallenge] = useState('')
+  const [rememberMe, setRememberMe] = useState(false)
   const [email, setEmail] = useState('')
-  const [password, setPasssword] = useState('')
+  const [password, setPassword] = useState('')
+  const [showPass, setShowPass] = useState(false)
 
   // handlers
   useEffect(() => {
     setCartData(shoppingCartData)
+    Auth.currentAuthenticatedUser()
+      .then(() => {
+        setIsAuthenticated(true)
+        router.push({ pathname: '/purchase', query: { tab: 1 } })
+      })
+      .catch(() => {
+        setIsAuthenticated(false)
+      })
+    setRememberMe(localStorage.getItem('remember'))
+    if (localStorage.getItem('remember')) {
+      setEmail(localStorage.getItem('email'))
+      setPassword(localStorage.getItem('password'))
+    }
   }, [])
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      router.push({ pathname: '/purchase', query: { tab: 1 } })
+    }
+  }, [isAuthenticated])
+
+  const handleSetRememberMe = () => {
+    setRememberMe(!rememberMe)
+  }
 
   const handleRemoveCart = index => {
     let array = [...cartData]
@@ -57,34 +89,58 @@ const PurchaseLogin = () => {
   }
 
   const handleChangePassword = event => {
-    setPasssword(event.target.value)
+    setPassword(event.target.value)
   }
 
   const handleClickLogin = () => {
-    console.log('handleClickLogin', email, password)
     router.push('/purchase-login')
   }
 
   const handleClickRegister = () => {
-    console.log('handleClickRegister')
     router.push('/purchase-register')
   }
 
   const handleClickGoogle = () => {
-    console.log('handleClickGoogle')
+    dispatch({ type: 'set', isLoading: true })
+    Auth.federatedSignIn({ provider: 'Google' })
+    dispatch({ type: 'set', isLoading: false })
   }
 
   const handleClickFacebook = () => {
-    console.log('handleClickFacebook')
+    dispatch({ type: 'set', isLoading: true })
+    Auth.federatedSignIn({ provider: 'Facebook' })
+    dispatch({ type: 'set', isLoading: false })
   }
 
-  const handleClickEnter = () => {
-    router.push({
-      pathname: '/purchase',
-      query: {
-        tab: 1,
-      },
-    })
+  const handleClickPurchaseLogin = async () => {
+    dispatch({ type: 'set', isLoading: true })
+    await Auth.signIn(email, password)
+      .then(response => {
+        setAuthChallenge(response.challengeName)
+        if (rememberMe) {
+          localStorage.setItem('email', email)
+          localStorage.setItem('password', password)
+          localStorage.setItem('remember', rememberMe)
+        } else {
+          // localStorage.setItem('email', '')
+          localStorage.setItem('password', '')
+          localStorage.setItem('remember', rememberMe)
+        }
+        localStorage.setItem('email', email)
+        if (response.challengeName !== 'NEW_PASSWORD_REQUIRED') {
+          toast.success('Successfully Logged in')
+          router.push({ pathname: '/purchase', query: { tab: 1 } })
+        }
+        dispatch({ type: 'set', isLoading: false })
+      })
+      .catch(error => {
+        dispatch({ type: 'set', isLoading: false })
+        toast.error(error.message)
+      })
+  }
+
+  const handleSetShowPass = bool => {
+    setShowPass(bool)
   }
 
   return (
@@ -92,47 +148,108 @@ const PurchaseLogin = () => {
       <div className={styles.container}>
         <div className={globalStyles.container + ' pt-20'}>
           <div className={'grid grid-cols-12 gap-4 '}>
-            <div className={'col-span-12 md:col-span-8 sm:col-span-12 pt-32 pb-44'}>
-              <div className={'px-10'}>
-                <div className={styles.title}>Regístrate para continuar con la compra</div>
+            {!authChallenge !== 'NEW_PASSWORD_REQUIRED' ? (
+              <div className={'col-span-12 md:col-span-8 sm:col-span-12 pt-32 pb-44'}>
+                <div className={'px-10'}>
+                  <div className={styles.title}>Regístrate para continuar con la compra</div>
+                </div>
+                <div className={'flex justify-center'}>
+                  <div className={'pt-12'} style={{ maxWidth: '300px', width: '100%' }}>
+                    <div className={'flex justify-start gap-4'}>
+                      <CommonButton handleClick={handleClickLogin} label={'LOGIN'} type={'fill'} />
+                      <CommonButton handleClick={handleClickRegister} label={'REGISTRO'} type={'outline'} />
+                    </div>
+                    <div className={'pt-9'}>
+                      <input
+                        type="email"
+                        placeholder="Email"
+                        className={styles.input}
+                        value={email}
+                        onChange={handleChangeEmail}
+                      />
+                    </div>
+                    <div className={'w-full relative flex items-center mt-5 ' + styles.inputArea}>
+                      <input
+                        type={showPass === true ? 'text' : 'password'}
+                        placeholder="Contraseña"
+                        className={styles.input}
+                        value={password}
+                        onChange={handleChangePassword}
+                      />
+                      <div className={'absolute right-3 cursor-pointer flex items-center'}>
+                        {showPass === true ? (
+                          <Image src={EyeIcon} alt="" width={17} height={17} onClick={() => handleSetShowPass(false)} />
+                        ) : (
+                          <Image
+                            src={EyeCrossIcon}
+                            alt=""
+                            width={17}
+                            height={17}
+                            onClick={() => handleSetShowPass(true)}
+                          />
+                        )}
+                      </div>
+                    </div>
+                    <div className={'flex justify-between items-center pt-6'}>
+                      <div className={'flex justify-between items-center'}>
+                        <input
+                          type="checkbox"
+                          style={{ width: 20, height: 20 }}
+                          onChange={handleSetRememberMe}
+                          checked={rememberMe}
+                        />
+                        <p className={styles.remember} onChange={handleSetRememberMe}>
+                          &nbsp;&nbsp;Recuerdame
+                        </p>
+                      </div>
+                      <div>
+                        <CommonButton handleClick={handleClickPurchaseLogin} label={'Entrar'} type={'fill'} />
+                      </div>
+                    </div>
+                    <div className={'pt-2 flex justify-end'}>
+                      <Link href={'/forgot-password'} passHref>
+                        <p className={styles.forgetPassword}>Olvidaste contraseña</p>
+                      </Link>
+                    </div>
+                    <div className={'flex justify-between items-center pt-10'}>
+                      <div className={styles.divider} />
+                      <div className={styles.remember}>or</div>
+                      <div className={styles.divider} />
+                    </div>
+                    <div className={'flex justify-between items-center pt-10'}>
+                      <CommonButton handleClick={handleClickFacebook} label={'LOGIN CON FACEBOOK'} type={'facebook'} />
+                    </div>
+                    <div className={'flex justify-between items-center pt-5'}>
+                      <CommonButton handleClick={handleClickGoogle} label={'LOGIN CON GOOGLE'} type={'google'} />
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className={'flex justify-center'}>
-                <div className={'pt-12'} style={{ maxWidth: '300px', width: '100%' }}>
-                  <div className={'flex justify-start gap-4'}>
-                    <CommonButton handleClick={handleClickLogin} label={'LOGIN'} type={'fill'} />
-                    <CommonButton handleClick={handleClickRegister} label={'REGISTRO'} type={'outline'} />
-                  </div>
-                  <div className={'pt-9'}>
+            ) : (
+              <div className={'col-span-12 md:col-span-8 sm:col-span-12 pt-32 pb-44'}>
+                <div>
+                  <div className={styles.updatePassword}>Actualiza contraseña</div>
+                  <div className={'w-full relative flex items-center mt-5 ' + styles.inputArea}>
                     <input
-                      type="email"
-                      placeholder="Email"
-                      className={styles.input}
-                      value={email}
-                      onChange={handleChangeEmail}
-                    />
-                  </div>
-                  <div className={'pt-5'}>
-                    <input
-                      type="password"
-                      placeholder="Contraseña"
-                      className={styles.input}
+                      type={showPass === true ? 'text' : 'password'}
+                      placeholder="Actualiza contraseña"
+                      className={'w-full h-full border border-white rounded bg-transparent py-1 pl-2 text-white pr-10'}
                       value={password}
                       onChange={handleChangePassword}
                     />
-                  </div>
-                  <div className={'flex justify-between items-center pt-6'}>
-                    <div className={'flex justify-between items-center'}>
-                      <input type="checkbox" style={{ width: 20, height: 20 }} />
-                      <p className={styles.remember}>&nbsp;&nbsp;Recuerdame</p>
+                    <div className={'absolute right-3 cursor-pointer flex items-center'}>
+                      {showPass === true ? (
+                        <Image src={EyeIcon} alt="" width={17} height={17} onClick={() => handleSetShowPass(false)} />
+                      ) : (
+                        <Image
+                          src={EyeCrossIcon}
+                          alt=""
+                          width={17}
+                          height={17}
+                          onClick={() => handleSetShowPass(true)}
+                        />
+                      )}
                     </div>
-                    <div>
-                      <CommonButton handleClick={handleClickEnter} label={'Entrar'} type={'fill'} />
-                    </div>
-                  </div>
-                  <div className={'pt-2 flex justify-end'}>
-                    <Link href={'/forgot-password'} passHref>
-                      <p className={styles.forgetPassword}>Olvidaste contraseña</p>
-                    </Link>
                   </div>
                   {/* <div className={'flex justify-between items-center pt-10'}>
                     <div className={styles.divider} />
@@ -145,9 +262,14 @@ const PurchaseLogin = () => {
                   <div className={'flex justify-between items-center pt-5'}>
                     <CommonButton handleClick={handleClickGoogle} label={'LOGIN CON GOOGLE'} type={'google'} />
                   </div> */}
+                  <div className={'mt-10 flex justify-end'}>
+                    <button className={styles.enterButton} onClick={handleUpdatePassword}>
+                      Entrar
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
+            )}
             <div className={'col-span-12 md:col-span-4 sm:col-span-12'}>
               <ShoppingCart data={cartData} handleRemoveCart={handleRemoveCart} />
             </div>
