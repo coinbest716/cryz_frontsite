e1b2d3090339cdec9f2b27ded0bae0ec496f3c4d
Luis Morales <luismmorales@gmail.com>
2021-12-15 05:36:39 -0800

    basic SEO and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Provider } from 'react-redux'
+import { useRouter } from 'next/router'
+import Head from 'next/head'
 
 // aws components
 import Amplify, { Auth } from 'aws-amplify'
@@ -23,11 +25,24 @@ import store from 'store.js'
 // styles
 import 'styles/style.scss'
 
+import { DefaultSeo } from 'next-seo'
+
 Amplify.configure({ ...awsconfig, ssr: true })
 Auth.configure(awsconfig)
 
 const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter()
   const getLayout = Component.getLayout || (page => page)
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   const [viewport, setViewport] = useState('desktop') // mobile, ipad, desktop
   useEffect(() => {
@@ -55,6 +70,38 @@ const MyApp = ({ Component, pageProps }) => {
 
   return (
     <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Fisioterapia, entrenamiento personal y mucho más" />
+        <meta name="keywords" content="Fisioterapia, entrenamiento personal y mucho más" />
+        <title>CrysDyaz&Co</title>
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+        <meta name="apple-mobile-web-app-title" content="CrysDyaz And Co"/>
+        <meta name="application-name" content="CrysDyaz And Co"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff" />
+
+      </Head>
+      <DefaultSeo
+          title="CrysDyaz&Co"
+          description="Fisioterapia, entrenamiento personal y mucho más"
+          openGraph={{
+            type: 'website',
+            locale: 'es_ES',
+            url: 'https://crysdyazandco.com/',
+            site_name: 'CrysDyaz&Co',
+          }}
+      />
       <ApolloProvider client={client}>
         <Provider store={store}>{getLayout(<Component {...pageProps} viewport={viewport} />)}</Provider>
         <Toaster position="top-right" reverseOrder={false} />
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 // pages/_document.js
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { Fragment } from 'react'
+import { GA_TRACKING_ID } from '../utils/gtag'
 
 class MyDocument extends Document {
   render() {
@@ -9,6 +10,22 @@ class MyDocument extends Document {
         <Head>
           <Fragment>
             <script type="text/javascript" src="https://cdn.iubenda.com/cons/iubenda_cons.js" async />
+
+              {/* Global Site Tag (gtag.js) - Google Analytics */}
+              <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+              <script
+                  dangerouslySetInnerHTML={{
+                      __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                  }}
+              />
+
           </Fragment>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Zilla+Slab:ital@1&display=swap" rel="stylesheet" />
