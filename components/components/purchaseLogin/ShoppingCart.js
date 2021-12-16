40f7af8111582cd9a1499cc07231bc7e7a3df221
luming63 <laomingri@gmail.com>
2021-12-16 12:03:50 +0100

    fixed: show bill address information in purchase page

--- components/components/purchaseLogin/ShoppingCart.js
+++ components/components/purchaseLogin/ShoppingCart.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react'
 
 // next components
 import Image from 'next/image'
+import Radio from 'components/components/purchase/Radio'
 
 // styles
 import styles from './ShoppingCart.module.scss'
 
 const ShoppingCart = props => {
-  const { shoppingInfo } = props
+  const { shoppingInfo, docData, handleChangeFrame } = props
 
   return (
     <div className={styles.shoppingCart}>
@@ -15,17 +16,37 @@ const ShoppingCart = props => {
         <div className={styles.summary}>Resumen compra</div>
       </div>
       <div className={styles.divider + ' mt-4 mb-4'} />
-      <div className={'max-h-54 overflow-y-auto -mr-8 pr-1'}>
-        <div className={'flex justify-between items-center my-3'}>
-          <div className={'flex justify-between'}>
-            <div className={'mr-4'} style={{ minWidth: '88px' }}>
-              <Image src={shoppingInfo?.image || '/images/main-mobile.png'} alt={''} width={88} height={88} />
-            </div>
-            <div className={'flex flex-col justify-evenly'}>
-              <div className={styles.listDescription}>{shoppingInfo?.description}</div>
-              <div className={styles.listPrice}>€&nbsp;&nbsp;{shoppingInfo?.price}</div>
+      <div className="flex flex-col justify-between">
+        <div className={'max-h-54 overflow-y-auto -mr-8 pr-1'}>
+          <div className={'flex justify-between items-center my-3'}>
+            <div className={'flex justify-between'}>
+              <div className={'mr-4'} style={{ minWidth: '88px' }}>
+                <Image src={shoppingInfo?.image || '/images/main-mobile.png'} alt={''} width={88} height={88} />
+              </div>
+              <div className={'flex flex-col justify-evenly'}>
+                <div className={styles.listDescription}>{shoppingInfo?.description}</div>
+                <div className={styles.listPrice}>€&nbsp;&nbsp;{shoppingInfo?.price}</div>
+              </div>
             </div>
           </div>
+        </div>
+        <div>
+          {docData && (
+            <div className={'pt-36'}>
+              <Radio
+                handleChangeType={handleChangeFrame}
+                type={'billAddress'}
+                value={'billAddress'}
+                label={'Dirección facturación'}
+              />
+              <div className={'pt-2 pl-8 ' + styles.billAddress}>
+                <div>{docData.name + ' ' + docData.surname}</div>
+                <div>{docData.address}</div>
+                <div>{docData.province}</div>
+                <div>{docData.postalCode}</div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
