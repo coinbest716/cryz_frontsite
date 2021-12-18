6ec52d86a244d7ce370f1d6d06d501a1c4f3fddb
luming63 <laomingri@gmail.com>
2021-12-18 17:07:40 +0100

    fixed: mobile service training page

---
+++ components/components/service/ServiceButton.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import Image from 'next/image'
+import styles from './ServiceButton.module.scss'
+import RightGrayIcon from 'public/images/arrow-right-gray.svg'
+
+const ServiceButton = props => {
+  const { label, onClick, type } = props
+
+  return (
+    <div>
+      <button className={styles.workWithUsButton + ' flex justify-between items-center w-full'} onClick={onClick}>
+        <p className={'mr-4 ' + styles.label}>{label}</p>
+        <Image src={RightGrayIcon} alt="" width={15} height={13} />
+      </button>
+    </div>
+  )
+}
+
+export default ServiceButton
