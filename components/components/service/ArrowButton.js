8a555bff4bf90db03d5fea69ef71f4874b49ec51
luming63 <laomingri@gmail.com>
2021-12-18 10:43:04 +0100

    fixed: mobile: physiotherapy page

--- components/components/service/ArrowButton.js
+++ components/components/service/ArrowButton.js
@@ -5,17 +5,30 @@ import ArrowRightUpGray from 'assets/images/arrow-right-up.svg'
 import BrownRightUpIcon from 'public/images/brown-arrow-right-up.svg'
 
 const ArrowButton = props => {
-  const { label, onClick, type } = props
+  const { label, onClick, type, mobile } = props
 
   return (
-    <button
-      className={styles.workWithUsButton + ' pl-28 pr-16 flex justify-around items-center w-full'}
-      onClick={onClick}
-    >
-      <p className={'mr-4 ' + styles.label}>{label}</p>
-      {type === 'nutrition' && <Image src={BrownRightUpIcon} alt="" width={36} height={30} />}
-      {type === 'physiotherapy' && <Image src={ArrowRightUpGray} alt="" width={36} height={30} />}
-    </button>
+    <div>
+      {mobile ? (
+        <button
+          className={styles.m_workWithUsButton + ' p-1 flex justify-around items-center w-full'}
+          onClick={onClick}
+        >
+          <p className={'mr-2 ' + styles.m_label}>{label}</p>
+          {type === 'nutrition' && <Image src={BrownRightUpIcon} alt="" width={22} height={20} />}
+          {type === 'physiotherapy' && <Image src={ArrowRightUpGray} alt="" width={22} height={20} />}
+        </button>
+      ) : (
+        <button
+          className={styles.workWithUsButton + ' pl-28 pr-16 flex justify-around items-center w-full'}
+          onClick={onClick}
+        >
+          <p className={'mr-4 ' + styles.label}>{label}</p>
+          {type === 'nutrition' && <Image src={BrownRightUpIcon} alt="" width={36} height={30} />}
+          {type === 'physiotherapy' && <Image src={ArrowRightUpGray} alt="" width={36} height={30} />}
+        </button>
+      )}
+    </div>
   )
 }
 
