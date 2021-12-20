c9d9a0b6a312e535dcb0f8fccf419431b6096fc4
luming63 <laomingri@gmail.com>
2021-12-20 16:44:23 +0100

    fixed: mobile code verification page

--- pages/reset-password/index.js
+++ pages/reset-password/index.js
@@ -10,6 +10,9 @@ import toast from 'react-hot-toast'
 import ReactLoading from 'react-loading'
 import EyeCrossIcon from 'assets/images/eye-cross.svg'
 import EyeIcon from 'assets/images/eye.svg'
+import EyeCrossGrayIcon from 'assets/images/eye-cross-gray.svg'
+import EyeGrayIcon from 'assets/images/eye-gray.svg'
+import { isMobile } from 'react-device-detect'
 
 import { Auth } from 'aws-amplify'
 
@@ -22,10 +25,15 @@ const ResetPassword = () => {
   const [verifyCode, setVerifyCode] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
+  const [mobile, setIsMobile] = useState(null)
 
   useEffect(() => {
     setEmail(localStorage.getItem('email'))
   }, [])
+
+  useEffect(() => {
+    setIsMobile(isMobile)
+  }, [isMobile])
 
   const handleSetShowPass = bool => {
     setShowPass(bool)
@@ -68,109 +76,204 @@ const ResetPassword = () => {
   return (
     <div className={'relative'}>
       <div className={'w-full h-screen flex grid grid-cols-12'}>
-        <div className={'w-full col-span-6 flex flex-wrap justify-center items-center ' + styles.whiteArea}>
-          <div className={styles.whiteAreaContent}>
-            <div styles={'w-full'}>
-              <Image src={LoginImage} alt="" width={484} height={416} />
-            </div>
-            <div className={'w-full text-center ' + styles.title}>El cambio comienza aqui</div>
-            <div className={'w-full text-center ' + styles.text}>Estás a un paso de cambiar tu contraseña</div>
-          </div>
-        </div>
-        <div className={'w-full col-span-6 flex flex-wrap justify-center items-center relative ' + styles.grayArea}>
-          <div className={styles.closeButton}>
-            <button
-              className={'duration-200 hover:bg-gray-300 rounded-full p-3 flex justify-center items-center'}
-              onClick={() => router.push('/')}
-            >
-              <Image src={CloseIcon} alt="" width={19} height={20} />
-            </button>
-          </div>
-          <div className={styles.grayAreaContent}>
-            <div className={'w-full flex justify-center'}>
-              <Image src={KeyImage} alt="" width={167} height={136} />
-            </div>
-            <div className={'w-full text-center ' + styles.changePassTitle}>Cambia la contraseña</div>
-            <div className={'w-full text-center mt-9 ' + styles.changePassText}>
-              Estás a un paso de cambiar tu contraseña
-            </div>
-            <div className={'w-full mt-9 ' + styles.inputArea}>
-              <input
-                id="email"
-                type="text"
-                autoComplete="new-password"
-                placeholder="Código de verificación"
-                className={'w-full h-full border border-white rounded bg-transparent py-1 px-2 text-white'}
-                value={email}
-                disabled={true}
-                onChange={handleChangeVerifyCode}
-              />
-            </div>
-            <div className={'w-full mt-9 ' + styles.inputArea}>
-              <input
-                id="verify-code"
-                type="text"
-                autoComplete="new-password"
-                placeholder="Código de verificación"
-                className={'w-full h-full border border-white rounded bg-transparent py-1 px-2 text-white'}
-                value={verifyCode}
-                onChange={handleChangeVerifyCode}
-              />
-            </div>
-            {/* password input */}
-            <div className={'w-full relative flex items-center mt-5 ' + styles.inputArea}>
-              <input
-                id="new-password"
-                type={showPass === true ? 'text' : 'password'}
-                autoComplete="new-password"
-                placeholder="Contraseña"
-                className={'w-full h-full border border-white rounded bg-transparent py-1 pl-2 text-white pr-10'}
-                value={password}
-                onChange={handleChangePassword}
-              />
-              <div className={'absolute right-3 cursor-pointer'}>
-                {showPass === true ? (
-                  <Image src={EyeIcon} alt="" width={17} height={17} onClick={() => handleSetShowPass(false)} />
-                ) : (
-                  <Image src={EyeCrossIcon} alt="" width={17} height={17} onClick={() => handleSetShowPass(true)} />
-                )}
+        {mobile ? (
+          <></>
+        ) : (
+          <div
+            className={
+              'w-full col-span-12 lg:col-span-6 md:col-span-6 flex-wrap justify-center items-center ' + styles.whiteArea
+            }
+          >
+            <div className={styles.whiteAreaContent}>
+              <div styles={'w-full'}>
+                <Image src={LoginImage} alt="" width={484} height={416} />
+              </div>
+              <div className={'w-full text-center ' + styles.title}>El cambio comienza aqui</div>
+              <div className={'w-full text-center ' + styles.text}>
+                Introduce el código de verificación recibido en tu email{' '}
               </div>
             </div>
-            {/* repeat password input */}
-            <div className={'w-full relative flex items-center mt-5 ' + styles.inputArea}>
-              <input
-                id="confirm-password"
-                autoComplete="new-password"
-                type={showRepeatPass === true ? 'text' : 'password'}
-                placeholder="Repetir contraseña"
-                className={'w-full h-full border border-white rounded bg-transparent py-1 pl-2 text-white pr-10'}
-                value={confirmPassword}
-                onChange={handleChangeConfirmPassword}
-              />
-              <div className={'absolute right-3 cursor-pointer'}>
-                {showRepeatPass === true ? (
-                  <Image src={EyeIcon} alt="" width={17} height={17} onClick={() => handleSetShowRepeatPass(false)} />
-                ) : (
-                  <Image
-                    src={EyeCrossIcon}
-                    alt=""
-                    width={17}
-                    height={17}
-                    onClick={() => handleSetShowRepeatPass(true)}
-                  />
-                )}
+          </div>
+        )}
+        {mobile ? (
+          <div
+            className={
+              'w-full p-10 col-span-12 lg:col-span-6 md:col-span-6 flex-wrap justify-center items-center relative ' +
+              styles.whiteArea
+            }
+          >
+            <div className="mt-24">
+              <div className={'w-full flex justify-center'}>
+                <Image src={KeyImage} alt="" width={167} height={136} />
               </div>
-            </div>
-            {/* login button part */}
+              <div className={'w-full text-center mt-9 ' + styles.m_changePassTitle}>Código de verificación</div>
+              <div className={'w-full text-center mt-9 ' + styles.m_changePassText}>
+                Introduce el código de verificación recibido en tu email
+              </div>
+              <div className={'w-full mt-16 ' + styles.inputArea}>
+                <input
+                  id="verify-code"
+                  type="text"
+                  autoComplete="new-password"
+                  placeholder="Código de verificación"
+                  className={'w-full h-full border border-gray-400 rounded bg-transparent py-1 px-2 text-gray-400'}
+                  value={verifyCode}
+                  onChange={handleChangeVerifyCode}
+                />
+              </div>
+              {/* password input */}
+              <div className={'w-full relative flex items-center mt-10 ' + styles.inputArea}>
+                <input
+                  id="new-password"
+                  type={showPass === true ? 'text' : 'password'}
+                  autoComplete="new-password"
+                  placeholder="Contraseña"
+                  className={
+                    'w-full h-full border border-gray-400 rounded bg-transparent py-1 pl-2 text-gray-400 pr-10'
+                  }
+                  value={password}
+                  onChange={handleChangePassword}
+                />
+                <div className={'absolute right-3 cursor-pointer items-center flex'}>
+                  {showPass === true ? (
+                    <Image src={EyeIcon} alt="" width={17} height={17} onClick={() => handleSetShowPass(false)} />
+                  ) : (
+                    <Image src={EyeCrossIcon} alt="" width={17} height={17} onClick={() => handleSetShowPass(true)} />
+                  )}
+                </div>
+              </div>
+              {/* repeat password input */}
+              <div className={'w-full relative flex items-center mt-5 ' + styles.inputArea}>
+                <input
+                  id="confirm-password"
+                  autoComplete="new-password"
+                  type={showRepeatPass === true ? 'text' : 'password'}
+                  placeholder="Repetir contraseña"
+                  className={
+                    'w-full h-full border border-gray-400 rounded bg-transparent py-1 pl-2 text-gray-400 pr-10'
+                  }
+                  value={confirmPassword}
+                  onChange={handleChangeConfirmPassword}
+                />
+                <div className={'absolute right-3 cursor-pointer flex items-center'}>
+                  {showRepeatPass === true ? (
+                    <Image
+                      src={EyeGrayIcon}
+                      alt=""
+                      width={17}
+                      height={17}
+                      onClick={() => handleSetShowRepeatPass(false)}
+                    />
+                  ) : (
+                    <Image
+                      src={EyeCrossGrayIcon}
+                      alt=""
+                      width={17}
+                      height={17}
+                      onClick={() => handleSetShowRepeatPass(true)}
+                    />
+                  )}
+                </div>
+              </div>
+              {/* login button part */}
 
-            {/* login button part */}
-            <div className={'mt-5 flex justify-end items-center'}>
-              <button className={styles.enterButton} onClick={handleResetPassword}>
-                Entrar
+              {/* login button part */}
+              <div className={'mt-10 flex justify-center items-center'}>
+                <button className={styles.m_enterButton} onClick={handleResetPassword} disabled={progressStatus}>
+                  Entrar
+                </button>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div
+            className={
+              'w-full col-span-12 lg:col-span-6 md:col-span-6 flex-wrap justify-center items-center relative ' +
+              styles.grayArea
+            }
+          >
+            <div className={styles.closeButton}>
+              <button
+                className={'duration-200 hover:bg-gray-300 rounded-full p-3 flex justify-center items-center'}
+                onClick={() => router.push('/')}
+              >
+                <Image src={CloseIcon} alt="" width={19} height={20} />
               </button>
             </div>
+            <div className={styles.grayAreaContent}>
+              <div className={'w-full flex justify-center'}>
+                <Image src={KeyImage} alt="" width={167} height={136} />
+              </div>
+              <div className={'w-full text-center ' + styles.changePassTitle}>Código de verificación</div>
+              <div className={'w-full text-center mt-9 ' + styles.changePassText}>
+                Introduce el código de verificación recibido en tu email
+              </div>
+              <div className={'w-full mt-9 ' + styles.inputArea}>
+                <input
+                  id="verify-code"
+                  type="text"
+                  autoComplete="new-password"
+                  placeholder="Código de verificación"
+                  className={'w-full h-full border border-white rounded bg-transparent py-1 px-2 text-white'}
+                  value={verifyCode}
+                  onChange={handleChangeVerifyCode}
+                />
+              </div>
+              {/* password input */}
+              <div className={'w-full relative flex items-center mt-5 ' + styles.inputArea}>
+                <input
+                  id="new-password"
+                  type={showPass === true ? 'text' : 'password'}
+                  autoComplete="new-password"
+                  placeholder="Contraseña"
+                  className={'w-full h-full border border-white rounded bg-transparent py-1 pl-2 text-white pr-10'}
+                  value={password}
+                  onChange={handleChangePassword}
+                />
+                <div className={'absolute right-3 cursor-pointer'}>
+                  {showPass === true ? (
+                    <Image src={EyeIcon} alt="" width={17} height={17} onClick={() => handleSetShowPass(false)} />
+                  ) : (
+                    <Image src={EyeCrossIcon} alt="" width={17} height={17} onClick={() => handleSetShowPass(true)} />
+                  )}
+                </div>
+              </div>
+              {/* repeat password input */}
+              <div className={'w-full relative flex items-center mt-5 ' + styles.inputArea}>
+                <input
+                  id="confirm-password"
+                  autoComplete="new-password"
+                  type={showRepeatPass === true ? 'text' : 'password'}
+                  placeholder="Repetir contraseña"
+                  className={'w-full h-full border border-white rounded bg-transparent py-1 pl-2 text-white pr-10'}
+                  value={confirmPassword}
+                  onChange={handleChangeConfirmPassword}
+                />
+                <div className={'absolute right-3 cursor-pointer'}>
+                  {showRepeatPass === true ? (
+                    <Image src={EyeIcon} alt="" width={17} height={17} onClick={() => handleSetShowRepeatPass(false)} />
+                  ) : (
+                    <Image
+                      src={EyeCrossIcon}
+                      alt=""
+                      width={17}
+                      height={17}
+                      onClick={() => handleSetShowRepeatPass(true)}
+                    />
+                  )}
+                </div>
+              </div>
+              {/* login button part */}
+
+              {/* login button part */}
+              <div className={'mt-5 flex justify-end items-center'}>
+                <button className={styles.enterButton} onClick={handleResetPassword}>
+                  Entrar
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
+        )}
       </div>
       {progressStatus && (
         <div className={globalStyles.loadingArea}>
