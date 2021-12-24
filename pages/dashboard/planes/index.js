86f5a54fe2f4820a515cb332e93faeb7e0722459
Beatriz <beatriz@gosuac.com>
2021-12-24 11:11:56 +0100

    Cambio de texto compra plan online

--- pages/dashboard/planes/index.js
+++ pages/dashboard/planes/index.js
@@ -422,13 +422,31 @@ const Planes = () => {
         </div>
       ) : (
         <div className={styles.imageContainer}>
-          <div className={styles.title + ' mt-48'}>
-            ESTAMOS PREPARANDO TU PLAN ONLINE PERSONALIZADO
-            <br />
-            EN BREVE ESTARÁ LISTO
-          </div>
-          <div className={'mt-14'}>
+
+<div className={''}>
             <Image src={PlansImage} width={274} height={226} />
+          </div>
+          <div className={styles.title + ' mt-20'}>
+            ESTAMOS PREPARANDO TU PLAN ONLINE PERSONALIZADO
+          </div>
+          <div className={'flex justify-center planonline_text'}>
+            <div className={globalStyles.container}>
+                <div className={styles.planonline_text}>
+                  
+                <p className='mt-20'>Recuerda:</p>
+                <ol>
+                  <li><span className='font-bold'>1. Rellenar el cuestionario inicial.</span> 
+                  Encontrarás cómo acceder a él desde tu dashboard.</li>  
+                  <li><span className='font-bold'>2. Introduce tus <span className='font-bold'>datos antropométricos</span> en tu perfil.</span></li>
+                </ol>
+                <p className='mt-5' ></p>
+                  <p className='mt-5'>Una vez lo hayas cumplimentado, en un plazo de 48/72h laborables recibirás tu plan mensual.</p>
+
+                  <p className='mt-5'>El siguiente paso será fijar una videollamada a través de la web para resolver todas las posibles dudas que 
+                  te hayan surgido al revisarlo.</p>
+
+                </div>
+              </div>
           </div>
           {/* <div className={styles.subTitle}>
             PUEDES RENOVAR TU BONO
