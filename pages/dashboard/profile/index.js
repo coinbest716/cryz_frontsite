2eac96cd6e6062bc9a341afb5603b1050dec1d0b
luming63 <laomingri@gmail.com>
2021-12-24 10:46:17 +0100

    fixed:  mobile some issues

--- pages/dashboard/profile/index.js
+++ pages/dashboard/profile/index.js
@@ -14,9 +14,7 @@ import Image from 'next/image'
 import SecondaryLayout from 'components/Layout/SecondaryLayout'
 import MobileDashboardLayout from 'components/Layout/MobileDashboardLayout'
 import NotificationButton from 'components/components/dashboard/NotificationButton'
-import PurchaseAvatar from 'components/components/purchase/PurchaseAvatar'
-import ProfileItem from 'components/components/dashboard/profile/ProfileItem'
-import ProfileMainButton from 'components/components/dashboard/profile/ProfileMainButton'
+import MobileMainProfile from 'components/components/dashboard/profile/MobileMainProfile'
 
 // import Profile from 'components/components/dashboard/Profile'
 import Personal from 'components/components/dashboard/Personal'
@@ -31,10 +29,6 @@ import graphql from 'crysdiazGraphql'
 import toast from 'react-hot-toast'
 import Modal from 'react-modal'
 import CloseIcon from 'public/images/close.svg'
-import ProfileIcon from 'public/images/profile_icon.svg'
-import ProfileAntro from 'public/images/profile_antro.svg'
-import ProfileBill from 'public/images/profile_bill.svg'
-import ProfileCompras from 'public/images/profile_compras.svg'
 
 const customStyles = {
   content: {
@@ -543,51 +537,13 @@ const Profile = props => {
         </div>
       </Modal>
       {viewport === 'mobile' ? (
-        <div className={styles.mobileContainer}>
-          <div className={'flex justify-start items-center mb-10'}>
-            <PurchaseAvatar avatar={personalInfo.avatar || ''} handleChangeAvatar={handleChangeAvatar} />
-            <div className={'pl-5'}>
-              <div className={styles.profileName}>{personalInfo.name + ' ' + personalInfo.surname}</div>
-              <div className={styles.profileCounry}>
-                {shippingInfo.province ? shippingInfo.province + ', ' + shippingInfo.country : shippingInfo.country}
-              </div>
-            </div>
-          </div>
-          <ProfileItem
-            image={ProfileIcon}
-            title="Datos"
-            index={0}
-            handleClickProfileItem={() => handleClickProfileItem('personal-data')}
-          />
-          <ProfileItem
-            image={ProfileAntro}
-            title="Antropometría"
-            index={1}
-            handleClickProfileItem={() => handleClickProfileItem('health')}
-          />
-          <ProfileItem
-            image={ProfileCompras}
-            title="Compras"
-            index={2}
-            handleClickProfileItem={() => handleClickProfileItem('compras')}
-          />
-          <ProfileItem
-            image={ProfileBill}
-            title="Direcciones facturación"
-            index={3}
-            handleClickProfileItem={() => handleClickProfileItem('bill-address')}
-          />
-          <div className="mt-16">
-            <ProfileMainButton label="LogOut" onClick={() => handleClickMainButton('logout')} type="logout" />
-          </div>
-          <div className="mt-6">
-            <ProfileMainButton
-              label="Borrar cuenta"
-              handleClickMainButton={() => handleClickMainButton('deleteAccount')}
-              type="deleteAccount"
-            />
-          </div>
-        </div>
+        <MobileMainProfile
+          handleClickProfileItem={handleClickProfileItem}
+          personalInfo={personalInfo}
+          handleClickMainButton={handleClickMainButton}
+          shippingInfo={shippingInfo}
+          handleChangeAvatar={handleChangeAvatar}
+        />
       ) : (
         <div className={'relative pt-10 pb-24 px-24 ' + styles.container}>
           <div className={'flex justify-between'}>
