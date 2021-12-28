eda7c2c7470077b4bb6a5e4d08671c55499359b5
luming63 <laomingri@gmail.com>
2021-12-28 04:33:14 +0100

    fixed: monile health header component in profile page

--- components/components/dashboard/profile/MobileHealthProfile.js
+++ components/components/dashboard/profile/MobileHealthProfile.js
@@ -6,14 +6,13 @@ import { useDispatch } from 'react-redux'
 // next components
 import { useRouter } from 'next/router'
 import Image from 'next/image'
-
-// custom components
-import MobileDashboardLayout from 'components/Layout/MobileDashboardLayout'
+import ArrowLeftWhite from 'public/images/arrow-left-white.svg'
 
 // styles
 import styles from './MobileHealthProfile.module.scss'
 
-const MobileHealthProfile = () => {
+const MobileHealthProfile = props => {
+  const { healthInfo, handleSaveMeasure, handleChangeHealth } = props
   // loading part ###########################
   const dispatch = useDispatch()
   const [isMounted, setIsMounted] = useState(false)
@@ -33,6 +32,27 @@ const MobileHealthProfile = () => {
   // variables
   const router = useRouter()
 
-  return <div>MobileHealthProfile</div>
+  const handleClickBack = () => {
+    router.push('/dashboard/profile#graphic', undefined, { shallow: true })
+  }
+
+  return (
+    <div>
+      <div className={styles.header + ' p-4'}>
+        <div
+          className="flex justify-start items-center w-fit cursor-pointer"
+          style={{ width: 'fit-content' }}
+          onClick={handleClickBack}
+        >
+          <Image src={ArrowLeftWhite} width={18} height={15} alt="" />
+          <div className={styles.backString + ' ml-2'}>Gráficos</div>
+        </div>
+        <div className="flex justify-end" onClick={handleSaveMeasure}>
+          <div className={styles.saveButton}>Aceptar cambios</div>
+        </div>
+        <div className={styles.title}>Editar información</div>
+      </div>
+    </div>
+  )
 }
 export default MobileHealthProfile
