e924bbed54aed34ffed5027f4f9419156e9f21a9
luming63 <laomingri@gmail.com>
2022-01-04 11:17:48 +0100

    fixed: mobile order failed page

--- pages/purchase/order-failed.js
+++ pages/purchase/order-failed.js
@@ -16,10 +16,11 @@ import styles from './order-success.module.scss'
 // images and icons
 import failedLogo from 'public/images/order-failed.png'
 
-const CreditSuccess = () => {
+const CreditSuccess = props => {
   // loading part ###########################
   const dispatch = useDispatch()
   const [isMounted, setIsMounted] = useState(false)
+  const { viewport } = props
 
   useEffect(() => {
     setIsMounted(true)
@@ -34,21 +35,43 @@ const CreditSuccess = () => {
   // loading part end #######################
 
   return (
-    <div className={'flex flex-wrap justify-center'}>
-      <div className={styles.container}>
-        <div className={globalStyles.container}>
-          <div className={'flex justify-center items-center h-full'}>
-            <div className={styles.creditSection}>
-              <div className={styles.title}>¡HA HABIDO UN PROBLEMA PROCESANDO TU PEDIDO! </div>
-              <div className={'pt-16 text-center'}>
-                <Image src={failedLogo} alt="" width={270} height={245} />
+    <>
+      {viewport === 'mobile' ? (
+        <div className={'flex flex-wrap justify-center'}>
+          <div className={styles.m_container}>
+            <div className={globalStyles.container}>
+              <div className={'flex justify-center h-full'}>
+                <div className={styles.creditSection}>
+                  <div className={'mt-14 ' + styles.m_title}>¡HA HABIDO UN PROBLEMA PROCESANDO TU PEDIDO! </div>
+                  <div className={'px-24 py-12 text-center'}>
+                    <Image src={failedLogo} alt="" width={270} height={245} />
+                  </div>
+                  <div className={styles.m_orderNumber}>INTÉNTALO DE NUEVO O PONTE EN CONTACTO CON NOSOTROS</div>
+                </div>
               </div>
-              <div className={'pt-16 ' + styles.orderNumber}>INTÉNTALO DE NUEVO O PONTE EN CONTACTO CON NOSOTROS</div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className={'flex flex-wrap justify-center'}>
+          <div className={styles.container}>
+            <div className={globalStyles.container}>
+              <div className={'flex justify-center items-center h-full'}>
+                <div className={styles.creditSection}>
+                  <div className={styles.title}>¡HA HABIDO UN PROBLEMA PROCESANDO TU PEDIDO! </div>
+                  <div className={'pt-16 text-center'}>
+                    <Image src={failedLogo} alt="" width={270} height={245} />
+                  </div>
+                  <div className={'pt-16 ' + styles.orderNumber}>
+                    INTÉNTALO DE NUEVO O PONTE EN CONTACTO CON NOSOTROS
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 export default CreditSuccess
