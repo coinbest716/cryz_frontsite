1fcdbf1499ec8be65b9d4111e08837f334c6586b
Nicholas Klave <nicholas.klave716@gmail.com>
2022-01-04 07:54:38 +0100

    Update Radio component.

--- components/components/Radio.js
+++ components/components/Radio.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Radio.module.scss'
 
 const Radio = props => {
-  const { handleChangeType, name, value, label } = props
+  const { handleChangeType, name, value, label, answer } = props
+  const [checked, setchecked] = useState('')
+
+  useEffect(() => {
+    if (answer === value) {
+      setchecked('checked')
+    } else {
+      setchecked('')
+    }
+  }, [answer])
 
   return (
     <div className={'flex'}>
-      <input type="radio" value={value} name={name} onChange={handleChangeType} className={styles.inputRadio} />
+      <input
+        type="radio"
+        value={value}
+        name={name}
+        checked={checked}
+        onChange={handleChangeType}
+        className={styles.inputRadio}
+      />
       <div>
         <p className={'pl-3 ' + styles.title}>{label}</p>
       </div>
