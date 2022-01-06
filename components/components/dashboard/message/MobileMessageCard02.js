3776ee5c5db7b03d795bcc24f4854de2a5f32a74
Nicholas Klave <nicholas.klave716@gmail.com>
2022-01-06 15:41:15 +0100

    Update MobileMessageCard02 component.

--- components/components/dashboard/message/MobileMessageCard02.js
+++ components/components/dashboard/message/MobileMessageCard02.js
@@ -10,25 +10,48 @@ import styles from 'components/components/dashboard/message/MobileMessageCard02.
 
 const MobileMessageCard02 = props => {
   const { message } = props
+  console.log(message)
   return (
-    <div className={'w-full py-5 px-7 flex justify-start'}>
-      <div className={styles.container}>
-        <div className={styles.content}>{message.content}</div>
-        <div className={'mt-4 flex justify-end items-center'}>
-          <div className={styles.time}>{message.create_date.slice(11, 16)}</div>
-        </div>
-        {message.attachment.length !== 0 ? (
-          message.attachment[0].type.split('/')[0] === 'image' ? (
-            <MessageImage02 message={message} />
-          ) : message.attachment[0].type.split('/')[0] === 'video' ? (
-            <MessageVideo02 message={message} />
+    <div>
+      <div className={'w-full pt-5 flex justify-start'}>
+        <div className={styles.container}>
+          <div className={styles.content}>{message.content}</div>
+          {message.attachment.length !== 0 ? (
+            message.attachment[0].type.split('/')[0] === 'image' ? (
+              <MessageImage02 message={message} />
+            ) : message.attachment[0].type.split('/')[0] === 'video' ? (
+              <MessageVideo02 message={message} />
+            ) : (
+              <MessageDownload02 message={message} />
+            )
           ) : (
-            <MessageDownload02 message={message} />
-          )
-        ) : (
-          <></>
-        )}
-        <div className={styles.triangle}></div>
+            <></>
+          )}
+        </div>
+      </div>
+      {/* avatar part */}
+      <div className={'w-full pb-5 flex justify-start'}>
+        <div className={styles.avatarContainer}>
+          <div className={'relative mr-4 ' + styles.avatar}>
+            {message.from_type === 'user'
+              ? message.from_name.split(' ')[0].slice(0, 1).toUpperCase()
+              : message.to_name.split(' ')[0].slice(0, 1).toUpperCase()}
+            {message.from_type === 'user'
+              ? message.from_name.split(' ')[1].slice(0, 1).toUpperCase()
+              : message.to_name.split(' ')[1].slice(0, 1).toUpperCase()}
+            {message.notification === 'unread' ? (
+              <div className={styles.status} style={{ backgroundColor: '#5D51C6' }}></div>
+            ) : (
+              <></>
+            )}
+          </div>
+          <div>
+            <div className={styles.content}>{message.from_name}</div>
+            <div className={'mt-4 flex justify-start items-center'}>
+              <div className={styles.time}>{message.create_date.slice(11, 16)}</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
