1649d4240ec4f39f03463b54b931efb7b5010aa2
luming63 <laomingri@gmail.com>
2022-01-11 08:32:39 +0100

    fixed: order detail new button click

--- components/components/dashboard/compras/ComprasButton.js
+++ components/components/dashboard/compras/ComprasButton.js
@@ -3,22 +3,22 @@ import React from 'react'
 import styles from './ComprasButton.module.scss'
 
 const ComprasButton = props => {
-  const { title, description, type } = props
+  const { title, description, type, onClick } = props
   return (
     <>
       {type === 'subscription' && (
-        <div className={styles.sub_container + ' flex justify-center itmes-center'}>
+        <div className={styles.sub_container + ' flex justify-center itmes-center'} onClick={onClick}>
           <div className={styles.sub_title}>{title}</div>
           <div className={styles.sub_description + ' ml-1'}>{description}</div>
         </div>
       )}
       {type === 'pending' && (
-        <div className={styles.pending_container + ' flex justify-center itmes-center'}>
+        <div className={styles.pending_container + ' flex justify-center itmes-center'} onClick={onClick}>
           <div className={styles.pending_title}>{title}</div>
         </div>
       )}
       {type === 'new' && (
-        <div className={styles.new_container + ' flex justify-center itmes-center'}>
+        <div className={styles.new_container + ' flex justify-center itmes-center'} onClick={onClick}>
           <div className={styles.new_title}>{title}</div>
         </div>
       )}
