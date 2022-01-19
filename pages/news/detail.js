26999950cfb8197716913e7c9001ba60f6302b00
fullstackdev63 <laomingri@gmail.com>
2022-01-19 09:45:41 +0100

    added: news initial detail, categoris page

---
+++ pages/news/detail.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+
+// redux
+import { useDispatch } from 'react-redux'
+
+// next components
+import Image from 'next/image'
+
+// custom components
+import PrimaryLayout from 'components/Layout/PrimaryLayout'
+
+const Detail = props => {
+  // loading part ###########################
+  const dispatch = useDispatch()
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+    return () => setIsMounted(false)
+  }, [])
+
+  useEffect(() => {
+    if (isMounted === true) {
+      dispatch({ type: 'set', isLoading: false })
+    }
+  }, [isMounted, dispatch])
+  // loading part end #######################
+
+  // variables
+  const { viewport } = props
+
+  return <>Detail Page</>
+}
+export default Detail
+
+Detail.getLayout = function getLayout(page) {
+  return <PrimaryLayout>{page}</PrimaryLayout>
+}
