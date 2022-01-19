0f1bf8e99dd6aaa077112d8004e192c22ed4e9fd
fullstackdev63 <laomingri@gmail.com>
2022-01-19 07:34:38 +0100

    fixed: initial news page

--- pages/news/index.js
+++ pages/news/index.js
@@ -8,6 +8,7 @@ import Image from 'next/image'
 
 // custom components
 import PrimaryLayout from 'components/Layout/PrimaryLayout'
+import CircularMark from 'components/components/CircularMark'
 
 // styles
 import globalStyles from 'styles/GlobalStyles.module.scss'
@@ -33,38 +34,84 @@ const News = props => {
   }, [isMounted, dispatch])
   // loading part end #######################
 
+  const mockupData = [
+    {
+      image:
+        'https://crysdiaz-public.s3.eu-west-1.amazonaws.com/attachment/ca1eaaac-9140-47a4-a580-290732e07834_1nutri.png',
+      title: 'LOREM IPSUM LOREM IPSUM LOREM IPSUM',
+      date: '2022-01-06T18:04:57.000Z',
+      description:
+        'Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum',
+    },
+    {
+      image:
+        'https://crysdiaz-public.s3.eu-west-1.amazonaws.com/attachment/ca1eaaac-9140-47a4-a580-290732e07834_1nutri.png',
+      title: 'LOREM IPSUM LOREM IPSUM LOREM IPSUM',
+      date: '2022-01-06T18:04:57.000Z',
+      description:
+        'Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum',
+    },
+    {
+      image:
+        'https://crysdiaz-public.s3.eu-west-1.amazonaws.com/attachment/ca1eaaac-9140-47a4-a580-290732e07834_1nutri.png',
+      title: 'LOREM IPSUM LOREM IPSUM LOREM IPSUM',
+      date: '2022-01-06T18:04:57.000Z',
+      description:
+        'Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum',
+    },
+  ]
+
   // variables
   const { viewport } = props
 
   return (
-    <div className={'flex flex-wrap justify-center'}>
-      <div className={styles.container}>
-        <div className={globalStyles.container + (viewport === 'mobile' ? ' pt-8' : ' pt-20')}>
-          <div className={'h-1/3 flex items-center'}>
-            <div>
-              <div className={styles.topTitle}>PRÓXIMAMENTE…</div>
-              <div className={styles.topDash + (viewport === 'mobile' ? ' mt-2' : ' mt-4')} />
-            </div>
-          </div>
-          <div className={'h-1/3 flex items-center justify-center'}>
-            <div>
-              <div className={'flex justify-center'}>
-                <Image
-                  src={news}
-                  width={viewport === 'mobile' ? 273 : 400}
-                  height={viewport === 'mobile' ? 52 : 93}
-                  alt=""
-                />
-              </div>
-
-              <div className={styles.description}>
-                Un espacio exclusivo dónde puedes consultar todas las novedades y últimas publicaciones de CrysDyaz&Co
+    <>
+      {mockupData.length > 0 ? (
+        <div className={'flex flex-wrap justify-center'}>
+          <div className={styles.container}>
+            <div className={globalStyles.container + ' mt-20'}>
+              <div className="flex justify-between mt-24">
+                <div>
+                  <div className={styles.newTopTitle}>News</div>
+                  <div className={styles.newTopDash + ' mt-5'} />
+                </div>
+                <CircularMark />
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className={'flex flex-wrap justify-center'}>
+          <div className={styles.container}>
+            <div className={globalStyles.container + (viewport === 'mobile' ? ' pt-8' : ' pt-20')}>
+              <div className={'h-1/3 flex items-center'}>
+                <div>
+                  <div className={styles.topTitle}>PRÓXIMAMENTE…</div>
+                  <div className={styles.topDash + (viewport === 'mobile' ? ' mt-2' : ' mt-4')} />
+                </div>
+              </div>
+              <div className={'h-1/3 flex items-center justify-center'}>
+                <div>
+                  <div className={'flex justify-center'}>
+                    <Image
+                      src={news}
+                      width={viewport === 'mobile' ? 273 : 400}
+                      height={viewport === 'mobile' ? 52 : 93}
+                      alt=""
+                    />
+                  </div>
+
+                  <div className={styles.description}>
+                    Un espacio exclusivo dónde puedes consultar todas las novedades y últimas publicaciones de
+                    CrysDyaz&Co
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 export default News
