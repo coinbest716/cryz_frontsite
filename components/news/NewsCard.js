6bb3b9b38fa1ff23664c44b313a6c05326c68835
fullstackdev63 <laomingri@gmail.com>
2022-01-21 07:37:34 +0100

    added: news page component, NewsCard, NewsCarousel

---
+++ components/news/NewsCard.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+
+// next components
+import Image from 'next/image'
+
+import moment from 'moment'
+// styles
+import styles from './NewsCard.module.scss'
+
+const NewsCard = props => {
+  const { item, handleClickDate } = props
+
+  let body = ''
+  body =
+    item.description.replace(/(<([^>]+)>)/gi, '').slice(0, 100) +
+    (item.description.replace(/(<([^>]+)>)/gi, '').length > 100 ? '...' : '')
+
+  return (
+    <div className={styles.cardContainer}>
+      <div className={styles.imageBorder}>
+        <Image
+          src={(item.images && item.images[0].path) || 'https://via.placeholder.com/365x253?text=Placeholder'}
+          width={365}
+          height={253}
+          alt=""
+          layout="responsive"
+          objectFit="cover"
+          objectPosition="center"
+        />
+      </div>
+      <div className={styles.textContainer + ' mt-2 py-3 px-5'}>
+        <div className={styles.cardTitle}>{item.title}</div>
+        <div className="flex justify-start">
+          <div className={styles.cardDate + ' mt-3 mb-2 cursor-pointer w-fit'} onClick={() => handleClickDate(item.id)}>
+            {moment(item.publish_date).format('DD/MM/YYYY')}
+          </div>
+        </div>
+        <div className={styles.cardDescription}>{body}</div>
+      </div>
+    </div>
+  )
+}
+
+export default NewsCard
