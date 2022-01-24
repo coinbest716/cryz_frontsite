4f87d30a766ded2aeffd716d26333cb712c5cc22
fullstackdev63 <laomingri@gmail.com>
2022-01-24 20:38:38 +0100

    fixed: Newscard, MobileNewsCard detect image

--- components/news/NewsCard.js
+++ components/news/NewsCard.js
@@ -19,7 +19,9 @@ const NewsCard = props => {
     <div className={styles.cardContainer}>
       <div className={styles.imageBorder}>
         <Image
-          src={(item.images && item.images[0]?.path) || 'https://via.placeholder.com/365x253?text=Placeholder'}
+          src={
+            (item.images.length > 0 && item.images[0]?.path) || 'https://via.placeholder.com/365x253?text=Placeholder'
+          }
           width={365}
           height={253}
           alt=""
