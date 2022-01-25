1cd1ae3ebb3be751a79b6382a0c326b5aafb5e4a
fullstackdev63 <laomingri@gmail.com>
2022-01-25 10:01:32 +0100

    fixed: workwithus page with mockupdata

--- pages/work-with-us/main.js
+++ pages/work-with-us/main.js
@@ -7,6 +7,7 @@ import { useDispatch } from 'react-redux'
 // custom components
 import PrimaryLayout from 'components/Layout/PrimaryLayout'
 import CircularMark from 'components/components/CircularMark'
+import WorkWithCard from 'components/workwithus/WorkWithCard'
 // styles
 import globalStyles from 'styles/GlobalStyles.module.scss'
 import styles from './main.module.scss'
@@ -34,21 +35,83 @@ const Main = props => {
   }, [isMounted, dispatch])
   // loading part end #######################
 
+  const mockupData = [
+    {
+      id: 1,
+      title: 'Nutricionista deportivo ',
+      description: 'Cras quis nulla commodo, aliquam lectus sed, blandit augue. ',
+    },
+
+    {
+      id: 2,
+      title: 'Nutricionista deportivo y entrenador personal',
+      description:
+        'Cras quis nulla commodo, aliquam lectus sed, blandit augue. Cras ullamcorper bibendum bibendum. Duis tincidunt urna non pretium porta. Nam condimentum vitae ligula vel ornare. Phasellus at semper turpis. Nunc eu tellus tortor. Etiam at condimentum nisl, vitae sagittis orci. Donec id dignissim nunc. Donec elit ante, eleifend a dolor et, venenatis facilisis dolor. In feugiat orci odio, sed lacinia sem elementum quis. Aliquam consectetur, eros et vulputate euismod, nunc leo tempor lacus, ac rhoncus neque eros nec lacus. Cras lobortis molestie faucibus.agittis orci. Donec id dignissim nunc. Donec elit ante, eleifend a dolor et, venenatis facilisis dolor. In feugiat orci odio, sed lacinia sem elementum quis. Aliquam consectetur, eros et vulputate euismod, nunc leo tempor lacus, ac rhoncus neque eros nec lacus. Cras lobortis molestie faucibus.',
+    },
+    {
+      id: 3,
+      title: 'Nutricionista deportivo y entrenador personal',
+      description:
+        'Cras quis nulla commodo, aliquam lectus sed, blandit augue. Cras ullamcorper bibendum bibendum. Duis tincidunt urna non pretium porta. Nam condimentum vitae ligula vel ornare. Phasellus at semper turpis. Nunc eu tellus tortor. Etiam at condimentum nisl, vitae sagittis orci. Donec id dignissim nunc. Donec elit ante, eleifend a dolor et, venenatis facilisis dolor. In feugiat orci odio, sed lacinia sem elementum quis. Aliquam consectetur, eros et vulputate euismod, nunc leo tempor lacus, ac rhoncus neque eros nec lacus. Cras lobortis molestie faucibus.agittis orci. Donec id dignissim nunc. Donec elit ante, eleifend a dolor et, venenatis facilisis dolor. In feugiat orci odio, sed lacinia sem elementum quis. Aliquam consectetur, eros et vulputate euismod, nunc leo tempor lacus, ac rhoncus neque eros nec lacus. Cras lobortis molestie faucibus.',
+    },
+    {
+      id: 3,
+      title: 'Nutricionista deportivo y entrenador personal',
+      description:
+        'Cras quis nulla commodo, aliquam lectus sed, blandit augue. Cras ullamcorper bibendum bibendum. Duis tincidunt urna non pretium porta. Nam condimentum vitae ligula vel ornare. Phasellus at semper turpis. Nunc eu tellus tortor. Etiam at condimentum nisl, vitae sagittis orci. Donec id dignissim nunc. Donec elit ante, eleifend a dolor et, venenatis facilisis dolor. In feugiat orci odio, sed lacinia sem elementum quis. Aliquam consectetur, eros et vulputate euismod, nunc leo tempor lacus, ac rhoncus neque eros nec lacus. Cras lobortis molestie faucibus.agittis orci. Donec id dignissim nunc. Donec elit ante, eleifend a dolor et, venenatis facilisis dolor. In feugiat orci odio, sed lacinia sem elementum quis. Aliquam consectetur, eros et vulputate euismod, nunc leo tempor lacus, ac rhoncus neque eros nec lacus. Cras lobortis molestie faucibus.',
+    },
+    {
+      id: 4,
+      title: 'Nutricionista deportivo y entrenador personal',
+      description:
+        'Cras quis nulla commodo, aliquam lectus sed, blandit augue. Cras ullamcorper bibendum bibendum. Duis tincidunt urna non pretium porta. Nam condimentum vitae ligula vel ornare. Phasellus at semper turpis. Nunc eu tellus tortor. Etiam at condimentum nisl, vitae sagittis orci. Donec id dignissim nunc. Donec elit ante, eleifend a dolor et, venenatis facilisis dolor. In feugiat orci odio, sed lacinia sem elementum quis. Aliquam consectetur, eros et vulputate euismod, nunc leo tempor lacus, ac rhoncus neque eros nec lacus. Cras lobortis molestie faucibus.agittis orci. Donec id dignissim nunc. Donec elit ante, eleifend a dolor et, venenatis facilisis dolor. In feugiat orci odio, sed lacinia sem elementum quis. Aliquam consectetur, eros et vulputate euismod, nunc leo tempor lacus, ac rhoncus neque eros nec lacus. Cras lobortis molestie faucibus.',
+    },
+    {
+      id: 5,
+      title: 'Nutricionista deportivo y entrenador personal',
+      description:
+        'Cras quis nulla commodo, aliquam lectus sed, blandit augue. Cras ullamcorper bibendum bibendum. Duis tincidunt urna non pretium porta. Nam condimentum vitae ligula vel ornare. Phasellus at semper turpis. Nunc eu tellus tortor. Etiam at condimentum nisl, vitae sagittis orci. Donec id dignissim nunc. Donec elit ante, eleifend a dolor et, venenatis facilisis dolor. In feugiat orci odio, sed lacinia sem elementum quis. Aliquam consectetur, eros et vulputate euismod, nunc leo tempor lacus, ac rhoncus neque eros nec lacus. Cras lobortis molestie faucibus.agittis orci. Donec id dignissim nunc. Donec elit ante, eleifend a dolor et, venenatis facilisis dolor. In feugiat orci odio, sed lacinia sem elementum quis. Aliquam consectetur, eros et vulputate euismod, nunc leo tempor lacus, ac rhoncus neque eros nec lacus. Cras lobortis molestie faucibus.',
+    },
+  ]
   // variables
   const { viewport } = props
+  const [mainInfo, setMainInfo] = useState([])
 
   // handlers
+  useEffect(() => {
+    setMainInfo(mockupData)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const handleClickDate = () => {
+    console.log('+++++++++++')
+  }
 
   return (
     <div className={'flex flex-wrap justify-center'}>
       <div className={styles.container}>
         <div className={globalStyles.container + ' my-20'}>
-          <div className="flex justify-between mt-24">
+          <div className="flex justify-between mt-24 mb-8">
             <div>
               <div className={styles.topTitle}>Trabaja con nosotros</div>
               <div className={styles.topDash + ' mt-5'} />
             </div>
             <CircularMark />
+          </div>
+          <div>
+            {mainInfo.length > 0 ? (
+              <div
+                className={
+                  'grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 xs:sm:grid-cols-1 gap-8 flex justify-center'
+                }
+              >
+                {mainInfo.map((item, index) => (
+                  <WorkWithCard item={item} key={index} handleClickDate={handleClickDate} />
+                ))}
+              </div>
+            ) : (
+              <></>
+            )}
           </div>
         </div>
       </div>
