9e04d5c5acd678b47ffb57458c9b013be073405c
fullstackdev63 <laomingri@gmail.com>
2022-01-27 07:47:27 +0100

    fixed: redirect when click confirm image

--- pages/work-with-us/confirm.js
+++ pages/work-with-us/confirm.js
@@ -1,7 +1,8 @@
-import React, { createRef, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 // redux
 import { useDispatch } from 'react-redux'
+import { useRouter } from 'next/router'
 
 // next components
 import Image from 'next/image'
@@ -17,6 +18,7 @@ import styles from './confirm.module.scss'
 import ConfirmImage from 'assets/images/confirm.png'
 
 const Confirm = () => {
+  const router = useRouter()
   // loading part ###########################
   const dispatch = useDispatch()
   const [isMounted, setIsMounted] = useState(false)
@@ -34,14 +36,8 @@ const Confirm = () => {
   // loading part end #######################
 
   // variables
-  const fileRef = createRef()
 
   // handlers
-  const handleAttachFile = event => {}
-
-  const onClickAttachFile = () => {
-    fileRef.current.click()
-  }
 
   return (
     <div className={'flex flex-wrap justify-center'}>
@@ -50,7 +46,12 @@ const Confirm = () => {
           <div className={'w-full flex justify-center ' + styles.title}>
             ¡Gracias por querer formar parte del equipo de Crys Dyaz!
           </div>
-          <div className={'w-full flex justify-center'}>
+          <div
+            className={'w-full flex justify-center cursor-pointer'}
+            onClick={() => {
+              router.push('/work-with-us')
+            }}
+          >
             <Image src={ConfirmImage} alt="" width={270} height={220} />
           </div>
           <div className="w-full text-cetner">
