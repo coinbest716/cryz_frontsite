0b3dd148e554c2b0631d45dc223e8674d916e202
fullstackdev63 <laomingri@gmail.com>
2022-01-28 09:05:48 +0100

    fixed: clean code maing page

--- pages/work-with-us/detail.js
+++ pages/work-with-us/detail.js
@@ -42,19 +42,11 @@ const Detail = props => {
   // loading part end #######################
 
   // variables
+  const { viewport } = props
   const [sendCV] = useMutation(graphql.mutations.SendCV)
   const [getJobByIdForDashboard, { data: withData, loading: withLoading, error: withError }] = useLazyQuery(
     graphql.queries.getJobByIdForDashboard
   )
-  const mockupData = {
-    id: 5,
-    title: 'Nutricionista deportivo y entrenador personal',
-    description:
-      'Cras quis nulla commodo, aliquam lectus sed, blandit augue. Cras ullamcorper bibendum bibendum. Duis tincidunt urna non pretium porta. Nam condimentum vitae ligula vel ornare. Phasellus at semper turpis. Nunc eu tellus tortor. Etiam at condimentum nisl, vitae sagittis orci. Donec id dignissim nunc. Donec elit ante, eleifend a dolor et, venenatis facilisis dolor. In feugiat orci odio, sed lacinia sem elementum quis. Aliquam consectetur, eros et vulputate euismod, nunc leo tempor lacus, ac rhoncus neque eros nec lacus. Cras lobortis molestie faucibus.agittis orci. Donec id dignissim nunc. Donec elit ante, eleifend a dolor et, venenatis facilisis dolor. In feugiat orci odio, sed lacinia sem elementum quis. Aliquam consectetur, eros et vulputate euismod, nunc leo tempor lacus, ac rhoncus neque eros nec lacus. Cras lobortis molestie faucibus.',
-    content:
-      'Cras quis nulla commodo, aliquam lectus sed, blandit augue. Cras ullamcorper bibendum bibendum. Duis tincidunt urna non pretium porta. Nam condimentum vitae ligula vel ornare. Phasellus at semper turpis. Nunc eu tellus tortor. Etiam at condimentum nisl, vitae sagittis orci. Donec id dignissim nunc. Donec elit ante, eleifend a dolor et, venenatis facilisis dolor. In feugiat orci odio, sed lacinia sem elementum quis. Aliquam consectetur, eros et vulputate euismod, nunc leo tempor lacus, ac rhoncus neque eros nec lacus. Cras lobortis molestie faucibus.agittis orci. Donec id dignissim nunc. Donec elit ante, eleifend a dolor et, venenatis facilisis dolor. In feugiat orci odio, sed lacinia sem elementum quis. Aliquam consectetur, eros et vulputate euismod, nunc leo tempor lacus, ac rhoncus neque eros nec lacus. Cras lobortis molestie faucibus.',
-  }
-  const { viewport } = props
   const fileRef = createRef()
   const [attachedFile, setAttachedFile] = useState({})
   const [personalInfo, setPersonalInfo] = useState({
