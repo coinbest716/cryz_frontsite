37e47242364cfcd371c94f85949c1f60803673f4
fullstackdev63 <laomingri@gmail.com>
2022-01-28 08:49:48 +0100

    fixed: mobile workwithus confirm page

--- pages/work-with-us/confirm.js
+++ pages/work-with-us/confirm.js
@@ -17,7 +17,7 @@ import styles from './confirm.module.scss'
 // images
 import ConfirmImage from 'assets/images/confirm.png'
 
-const Confirm = () => {
+const Confirm = props => {
   const router = useRouter()
   // loading part ###########################
   const dispatch = useDispatch()
@@ -36,31 +36,57 @@ const Confirm = () => {
   // loading part end #######################
 
   // variables
-
+  const { viewport } = props
   // handlers
 
   return (
-    <div className={'flex flex-wrap justify-center'}>
-      <div className={styles.container}>
-        <div className={globalStyles.container + ' my-20'}>
-          <div className={'w-full flex justify-center ' + styles.title}>
-            ¡Gracias por querer formar parte del equipo de Crys Dyaz!
-          </div>
-          <div
-            className={'w-full flex justify-center cursor-pointer'}
-            onClick={() => {
-              router.push('/work-with-us')
-            }}
-          >
-            <Image src={ConfirmImage} alt="" width={270} height={220} />
-          </div>
-          <div className="w-full text-cetner">
-            <div className={'w-full ' + styles.textOne}>¡Tu solicitud ha sido recibida con éxito!</div>
-            <div className={'w-full ' + styles.textTwo}>Si nos encaja tu perfil, nos pondremos en contacto contigo</div>
+    <>
+      {viewport === 'mobile' ? (
+        <div className={styles.m_container}>
+          <div className="w-full p-10 mb-5 mt-10">
+            <div className={'w-full flex justify-center ' + styles.m_title}>
+              ¡Gracias por querer formar parte del equipo de Crys Dyaz!
+            </div>
+            <div
+              className={'w-full flex justify-center cursor-pointer p-10'}
+              onClick={() => {
+                router.push('/work-with-us')
+              }}
+            >
+              <Image src={ConfirmImage} alt="" width={200} height={160} />
+            </div>
+            <div className={'w-full text-cetner ' + styles.m_textOne}>
+              ¡Tu solicitud ha sido recibida con éxito! <br />
+              Si queda libre una vacante y nos encaja tu perfil, nos pondremos en contacto contigo
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className={'flex flex-wrap justify-center'}>
+          <div className={styles.container}>
+            <div className={globalStyles.container + ' my-20'}>
+              <div className={'w-full flex justify-center ' + styles.title}>
+                ¡Gracias por querer formar parte del equipo de Crys Dyaz!
+              </div>
+              <div
+                className={'w-full flex justify-center cursor-pointer'}
+                onClick={() => {
+                  router.push('/work-with-us')
+                }}
+              >
+                <Image src={ConfirmImage} alt="" width={270} height={220} />
+              </div>
+              <div className="w-full text-cetner">
+                <div className={'w-full ' + styles.textOne}>¡Tu solicitud ha sido recibida con éxito!</div>
+                <div className={'w-full ' + styles.textTwo}>
+                  Si nos encaja tu perfil, nos pondremos en contacto contigo
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
