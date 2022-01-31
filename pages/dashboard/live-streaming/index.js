f8d5bf623b87a3bd7aa90901a1035eacf43fcddc
Nicholas Klave <nicholas.klave716@gmail.com>
2022-01-31 09:23:36 +0100

    Remove material part from  live-streaming page when the type is 'academy'.

--- pages/dashboard/live-streaming/index.js
+++ pages/dashboard/live-streaming/index.js
@@ -41,6 +41,7 @@ const LiveStreaming = props => {
   const router = useRouter()
   const [room, setRoom] = useState('')
   const [event, setEvent] = useState({ id: -1, start: '' })
+  const [type, setType] = useState('')
   const [material, setMaterial] = useState([])
   const [
     getSessionMaterialByDashboard,
@@ -52,6 +53,7 @@ const LiveStreaming = props => {
     if (router.query.id) {
       setEvent({ id: router.query.id })
     }
+    setType(router.query.type)
   }, [router.query])
 
   useEffect(() => {
@@ -77,28 +79,31 @@ const LiveStreaming = props => {
 
   return (
     <div className={'pt-7 lg:pl-14 lg:pr-8 ' + styles.container}>
+      {console.log('========type', type)}
       <div className={'grid grid-cols-12 gap-6'}>
-        <div className={'col-span-12 lg:col-span-9'}>
+        <div className={type !== 'academy' ? 'col-span-12 lg:col-span-9' : 'col-span-12'}>
           {viewport !== 'mobile' && <div className={styles.title}>1 to 1 Streaming</div>}
           <div className={'pt-4 lg:pt-14'}>
             <VideoChat sessionId={event.id} viewport={viewport} onChangeRoom={room => handleOnChangeRoom(room)} />
           </div>
         </div>
-        <div className={'col-span-12 md:col-span-3 sm:col-span-12'}>
-          <div className={'rounded-xl bg-white py-8 px-8 pb-10 mt-10'}>
-            <div className={styles.material}>Material necesario</div>
-            <div className={'pt-7'}>
-              {material.map((item, index) => (
-                <div className={'flex justify-start py-2'} key={index}>
-                  <div className={'pl-4 flex flex-col justify-around'}>
-                    <div className={styles.label}>{item.name}</div>
-                    <div className={styles.count}>{item.details}</div>
+        {type !== 'academy' && (
+          <div className={'col-span-12 md:col-span-3 sm:col-span-12'}>
+            <div className={'rounded-xl bg-white py-8 px-8 pb-10 mt-10'}>
+              <div className={styles.material}>Material necesario</div>
+              <div className={'pt-7'}>
+                {material.map((item, index) => (
+                  <div className={'flex justify-start py-2'} key={index}>
+                    <div className={'pl-4 flex flex-col justify-around'}>
+                      <div className={styles.label}>{item.name}</div>
+                      <div className={styles.count}>{item.details}</div>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   )
