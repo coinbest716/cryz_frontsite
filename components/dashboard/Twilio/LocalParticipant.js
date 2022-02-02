79bc33d00b082a2473dd28eacbf28900c60958b3
Nicholas Klave <nicholas.klave716@gmail.com>
2022-02-02 09:39:35 +0100

    Fix some issue in Twilio component.

--- components/dashboard/Twilio/LocalParticipant.js
+++ components/dashboard/Twilio/LocalParticipant.js
@@ -1,21 +1,20 @@
 import React, { useState, useEffect, useRef } from 'react'
 
-const SelfVideo = props => {
+const LocalParticipant = props => {
   // variables
-  const { participant } = props
-
+  const { participant, viewport, cameraEnabled, audioEnabled } = props
   const [videoTracks, setVideoTracks] = useState([])
   const [audioTracks, setAudioTracks] = useState([])
 
   const videoRef = useRef()
   const audioRef = useRef()
 
+  // handlers
   const trackpubsToTracks = trackMap =>
     Array.from(trackMap.values())
       .map(publication => publication.track)
       .filter(track => track !== null)
 
-  // handlers
   useEffect(() => {
     setVideoTracks(trackpubsToTracks(participant.videoTracks))
     setAudioTracks(trackpubsToTracks(participant.audioTracks))
@@ -66,12 +65,47 @@ const SelfVideo = props => {
     }
   }, [audioTracks])
 
+  useEffect(() => {
+    const videoTrack = videoTracks[0]
+    if (videoRef) {
+      if (videoTrack) {
+        if (cameraEnabled) {
+          videoTrack.enable()
+        } else {
+          videoTrack.disable()
+        }
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cameraEnabled])
+
+  useEffect(() => {
+    const audioTrack = audioTracks[0]
+    if (audioTrack) {
+      if (audioEnabled) {
+        audioTrack.enable()
+      } else {
+        audioTrack.disable()
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [audioEnabled])
+
   return (
-    <div className="w-full h-full flex justify-center">
-      <video className="w-full h-full" ref={videoRef} autoPlay={true} />
+    <div
+      className="flex justify-center mt-1 bg-black"
+      style={{
+        border: '1px solid #C4C4C4',
+      }}
+    >
+      <video
+        style={{ width: viewport === 'mobile' ? '120px' : '240px', height: viewport === 'mobile' ? '67px' : '134px' }}
+        ref={videoRef}
+        autoPlay={true}
+      />
       <audio ref={audioRef} autoPlay={true} muted={false} />
     </div>
   )
 }
 
-export default SelfVideo
+export default LocalParticipant
