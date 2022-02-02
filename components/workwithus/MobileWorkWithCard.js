6942d161392b32f62209a92d4d27e224caa81390
fullstackdev63 <laomingri@gmail.com>
2022-02-02 10:28:47 +0100

    fixed: dangerousSetInnerHtml with description

--- components/workwithus/MobileWorkWithCard.js
+++ components/workwithus/MobileWorkWithCard.js
@@ -5,26 +5,23 @@ import Image from 'next/image'
 
 import ArrowRightGray from 'public/images/arrow-right-gray.svg'
 // styles
+import globalStyles from 'styles/GlobalStyles.module.scss'
 import styles from './MobileWorkWithCard.module.scss'
 
 const MobileWorkWithCard = props => {
   const { item, handleClickOffer } = props
   let title = ''
-  let body = ''
   title =
     item.title.replace(/(<([^>]+)>)/gi, '').slice(0, 60) +
     (item.title.replace(/(<([^>]+)>)/gi, '').length > 60 ? '...' : '')
-  body =
-    item.description.replace(/(<([^>]+)>)/gi, '').slice(0, 320) +
-    (item.description.replace(/(<([^>]+)>)/gi, '').length > 320 ? '...' : '')
 
   return (
     <div style={{ maxWidth: '365px', minHeight: '289px' }} className={styles.cardContainer + ' p-4'}>
       <div className={styles.title + ' mt-3'} style={{ minHeight: '42px' }}>
         {title}
       </div>
-      <div className={styles.description + ' mt-4'} style={{ minHeight: '142px' }}>
-        {body}
+      <div className={globalStyles.tinyMCEClass + ' mt-4'} style={{ minHeight: '142px' }}>
+        <div className={styles.description + ' tinymce-class'} dangerouslySetInnerHTML={{ __html: item.description }} />
       </div>
 
       <div
