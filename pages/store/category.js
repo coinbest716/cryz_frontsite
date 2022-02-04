4bfd56682a266b05fd45fde0ef76d04e8230c444
fullstackdev63 <laomingri@gmail.com>
2022-02-04 13:17:01 +0100

    fixed: Store Category Page

--- pages/store/category.js
+++ pages/store/category.js
@@ -7,7 +7,8 @@ import { useRouter } from 'next/router'
 
 // custom components
 import PrimaryLayout from 'components/Layout/PrimaryLayout'
-import BackButton from 'components/components/BackButton'
+import StoreProductCard from 'components/store/StoreProductCard'
+import StorePregress from 'components/store/StorePregress'
 
 import { useMutation, useLazyQuery } from '@apollo/client'
 import graphql from 'crysdiazGraphql'
@@ -16,6 +17,7 @@ import toast from 'react-hot-toast'
 // styles
 import globalStyles from 'styles/GlobalStyles.module.scss'
 import styles from './category.module.scss'
+import { setHubOnCarrier } from '@sentry/hub'
 
 const Category = props => {
   const router = useRouter()
@@ -41,9 +43,94 @@ const Category = props => {
     { id: 2, label: 'Salud y Belleza' },
     { id: 3, label: 'Productos Bio' },
   ]
+
+  const newProduct = [
+    {
+      id: 0,
+      url: '/images/accessory.svg',
+      name: 'Poke flannel marfa swag slow-carb narwhal',
+      price: '265',
+      discountPrice: '154',
+    },
+    {
+      id: 1,
+      url: '/images/accessory.svg',
+      name: 'Poke flannel marfa swag slow-carb narwhal',
+      price: '265',
+      discountPrice: '',
+    },
+    {
+      id: 2,
+      url: '/images/accessory.svg',
+      name: 'Poke flannel marfa swag slow-carb narwhal',
+      price: '265',
+      discountPrice: '154',
+    },
+    {
+      id: 3,
+      url: '/images/accessory.svg',
+      name: 'Poke flannel marfa swag slow-carb narwhal',
+      price: '265',
+      discountPrice: '',
+    },
+    {
+      id: 4,
+      url: '/images/accessory.svg',
+      name: 'Poke flannel marfa swag slow-carb narwhal',
+      price: '265',
+      discountPrice: '154',
+    },
+    {
+      id: 5,
+      url: '/images/accessory.svg',
+      name: 'Poke flannel marfa swag slow-carb narwhal',
+      price: '265',
+      discountPrice: '',
+    },
+    {
+      id: 6,
+      url: '/images/accessory.svg',
+      name: 'Poke flannel marfa swag slow-carb narwhal',
+      price: '265',
+      discountPrice: '154',
+    },
+    {
+      id: 7,
+      url: '/images/accessory.svg',
+      name: 'Poke flannel marfa swag slow-carb narwhal',
+      price: '265',
+      discountPrice: '',
+    },
+    {
+      id: 8,
+      url: '/images/accessory.svg',
+      name: 'Poke flannel marfa swag slow-carb narwhal',
+      price: '265',
+      discountPrice: '',
+    },
+    {
+      id: 9,
+      url: '/images/accessory.svg',
+      name: 'Poke flannel marfa swag slow-carb narwhal',
+      price: '265',
+      discountPrice: '154',
+    },
+    {
+      id: 10,
+      url: '/images/accessory.svg',
+      name: 'Poke flannel marfa swag slow-carb narwhal',
+      price: '265',
+      discountPrice: '',
+    },
+  ]
+
   // variables
   const { viewport } = props
+  const [products, setProducts] = useState([])
+  const [someProducts, setSomeProducts] = useState([])
   const [categoryId, setCategoryId] = useState(-1)
+  const [showCount, setShowCount] = useState(0)
+  const [moreToggle, setMoreToggle] = useState(false)
 
   // handlers
   useEffect(() => {
@@ -52,14 +139,36 @@ const Category = props => {
     }
     if (router.query.category) {
       setCategoryId(router.query.category)
-      console.log(categoryList[router.query.category].label)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [router.query])
 
   useEffect(() => {
+    setProducts(newProduct)
+    console.log('#############', newProduct)
+    if (newProduct.length > 9) {
+      setShowCount(9)
+      setMoreToggle(true)
+      setSomeProducts(newProduct.slice(0, 9))
+    } else if (newProduct.length >= 0) {
+      setShowCount(newProduct.length)
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  const handleClickProduct = id => {
+    router.push({
+      pathname: '/store/detail',
+      query: {
+        id: id,
+      },
+    })
+  }
+
+  const handleClickMore = () => {
+    setMoreToggle(!moreToggle)
+    setShowCount(products.length)
+  }
 
   return (
     <>
@@ -74,6 +183,39 @@ const Category = props => {
                 <p className={styles.homePath}>&nbsp;{'>'}&nbsp;</p>
                 <p className={styles.categoryPath + ' cursor-pointer'}>{categoryList[categoryId]?.label}</p>
               </div>
+              <div className="flex justify-end mt-10">
+                <div className={'flex justify-between items-center ' + styles.filterContainer}>
+                  <p className={styles.filter}>Filtrar por</p>
+                  <Image src={'/images/filter-gray.svg'} alt="" width={17} height={14} />
+                </div>
+              </div>
+              <div className={'grid grid-cols-3 gap-8 flex justify-center mt-10'}>
+                {moreToggle ? (
+                  <>
+                    {someProducts.map((item, index) => (
+                      <StoreProductCard item={item} key={index} handleClickProduct={handleClickProduct} />
+                    ))}
+                  </>
+                ) : (
+                  <>
+                    {products.map((item, index) => (
+                      <StoreProductCard item={item} key={index} handleClickProduct={handleClickProduct} />
+                    ))}
+                  </>
+                )}
+              </div>
+              <div className="flex justify-center mt-20">
+                <StorePregress currentCount={showCount} allCount={products.length} />
+              </div>
+              {moreToggle ? (
+                <div className="flex justify-center mt-8 cursor-pointer">
+                  <div className={styles.moreButton} onClick={handleClickMore}>
+                    Cargar mas
+                  </div>
+                </div>
+              ) : (
+                <></>
+              )}
             </div>
           </div>
         </div>
