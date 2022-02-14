03971a67aad8596319380ca7511f26c776860af3
Nicholas Klave <nicholas.klave716@gmail.com>
2022-02-14 15:52:06 +0100

    Update StoreProductCard component.

--- components/Store/StoreProductCard.js
+++ components/Store/StoreProductCard.js
@@ -7,9 +7,10 @@ import Image from 'next/image'
 import styles from './StoreProductCard.module.scss'
 
 const StoreProductCard = props => {
-  const { item, handleClickProduct } = props
+  const { item, handleClickProduct, viewport } = props
+  console.log('viewport', viewport)
 
-  return (
+  return viewport !== 'mobile' ? (
     <div className="cursor-pointer" style={{ maxWidth: '370px' }} onClick={() => handleClickProduct(item.id)}>
       <div className={styles.newImage}>
         <Image src={item.url} alt="" width={370} height={460} />
@@ -23,6 +24,23 @@ const StoreProductCard = props => {
           </>
         ) : (
           <div className={styles.orgPrice}>{item.price}€</div>
+        )}
+      </div>
+    </div>
+  ) : (
+    <div className="cursor-pointer w-full" onClick={() => handleClickProduct(item.id)}>
+      <div className={styles.newImage}>
+        <Image src={item.url} alt="" width={370} height={460} />
+      </div>
+      <div className={styles.mobileNewName + ' my-2'}>{item.name}</div>
+      <div className="flex flex-wrap ">
+        {item.discountPrice ? (
+          <>
+            <div className={'line-through mr-3 ' + styles.mobileOrgPrice}>{item.price}€</div>
+            <div className={styles.mobileDisPrice}>{item.discountPrice}€</div>
+          </>
+        ) : (
+          <div className={styles.mobileOrgPrice}>{item.price}€</div>
         )}
       </div>
     </div>
